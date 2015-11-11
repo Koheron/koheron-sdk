ba1151a53f8e308716cf1642e7a7daadca45e290
jeanminet <jean.minet@gmail.com>
2015-11-11 18:13:06 +0100

    add properties procedure

--- boards/red-pitaya/base_system.tcl
+++ boards/red-pitaya/base_system.tcl
@@ -1,25 +1,23 @@
 set board_preset boards/$board_name/config/board_preset.xml
 
 # Create processing_system7
-cell xilinx.com:ip:processing_system7:5.5 ps_0 [list PCW_IMPORT_BOARD_PRESET $board_preset PCW_USE_S_AXI_HP0 0] [list M_AXI_GP0_ACLK ps_0/FCLK_CLK0]
+set ps_name ps_0
+cell xilinx.com:ip:processing_system7:5.5 $ps_name [list PCW_IMPORT_BOARD_PRESET $board_preset PCW_USE_S_AXI_HP0 0] [list M_AXI_GP0_ACLK ps_0/FCLK_CLK0]
 
 # Create all required interconnections
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
   make_external {FIXED_IO, DDR}
   Master Disable
   Slave Disable
-} [get_bd_cells ps_0]
+} [get_bd_cells $ps_name]
 
 # Add XADC Wizard and AXI Interconnect
 set xadc_name xadc_wiz_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:xadc_wiz:3.2 $xadc_name
 
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
-  Master "/ps_0/M_AXI_GP0"
-  Clk "Auto" 
-} [get_bd_intf_pins $xadc_name/s_axi_lite]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config [list Master "/${ps_name}/M_AXI_GP0" Clk "Auto"] [get_bd_intf_pins $xadc_name/s_axi_lite]
 
-set xadc_props {
+properties $xadc_name {
   XADC_STARUP_SELECTION independent_adc
   OT_ALARM false
   USER_TEMP_ALARM false
@@ -34,12 +32,22 @@ set xadc_props {
   CHANNEL_ENABLE_VAUXP9_VAUXN9 true
   CHANNEL_ENABLE_VP_VN true
 }
-foreach {prop_name prop_value} $xadc_props {
-  lappend prop_list CONFIG.$prop_name $prop_value
-}
-set_property -dict $prop_list [get_bd_cells $xadc_name]
 
-set xadc_ports {Vp_Vn Vaux0 Vaux1 Vaux8 Vaux9}
-foreach {port_name} $xadc_ports {
+foreach {port_name} {
+  Vp_Vn
+  Vaux0
+  Vaux1
+  Vaux8
+  Vaux9
+} {
   connect_bd_intf_net [get_bd_intf_pins $xadc_name/$port_name] [get_bd_intf_ports $port_name]
 }
+
+# Number of Master Interfaces in AXI Interconnect
+set num_mi 1
+set axi_interconnect_name ${ps_name}_axi_periph
+
+# Add dual-port BRAM
+set num_mi [expr $num_mi+1]
+properties ps_0_axi_periph [list NUM_MI $num_mi]
+
