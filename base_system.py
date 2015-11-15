07aa375f853b71aa4938e91f68d4a93a062d821f
Jean Minet <jean.minet@gmail.com>
2015-11-15 20:45:06 +0100

    add tests to base_system.py

--- base_system.py
+++ base_system.py
@@ -1,22 +1,42 @@
 from lase.core import KClient, DevMem
 import numpy as np
-import matplotlib.pyplot as plt
-import time
 
 host = '192.168.1.12'
 client = KClient(host)
 dvm = DevMem(client)
 
-_config_addr = int('0x60000000',0)
-_gpio_addr = int('0x41200000',0)
+# Base address of configuration register
+CONFIG_ADDR = int('0x60000000',0)
+# Offsets of configuration register
+LED  = 0
+PWM0 = 4
+PWM1 = 8
+PWM2 = 12
+PWM3 = 16
 
-_led_offset = 0
-_pwm_offset = 4
+# Base address of AXI GPIO
+GPIO_ADDR = int('0x41200000',0)
+# GPIO registers offsets (https://github.com/Koheron/zynq-sdk/issues/13)
+GPIO_DATA  = 0
+GPIO_TRI   = 4
+GPIO2_DATA = 8
+GPIO2_TRI  = 12
 
-config = dvm.add_memory_map(_config_addr, 1*4096)
-gpio = dvm.add_memory_map(_gpio_addr, 16*4096)
+# Ask Koheron server to open memory maps
+CONFIG = dvm.add_memory_map(CONFIG_ADDR, 1*4096)
+GPIO = dvm.add_memory_map(GPIO_ADDR, 16*4096)
 
-dvm.write(gpio, 0, 255)
 
-dvm.write(config, _led_offset, 11)
-dvm.write(config, _pwm_offset, 512)
+# Set all GPIOs of channel 1 to output 
+dvm.write(GPIO, GPIO_TRI, 0)
+
+# Turn ON (3.3 V) the first 4 GPIOs of channel 1
+dvm.write(GPIO, GPIO_DATA, 15)
+
+# Turn on LEDs 0, 1 and 3
+dvm.write(CONFIG, LED, 11)
+
+# Set Analog Output 0 to 0.9 V
+dvm.write(CONFIG, PWM0, 512)
+
+
