35337a2e4bd18553763c963e048cd937f6c1b459
Jean Minet <jean.minet@gmail.com>
2015-12-09 19:54:26 +0100

    Update ubuntu.sh

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -130,7 +130,7 @@ iface wlan0 inet static
   post-up service isc-dhcp-server restart
   post-up iptables-restore < /etc/iptables.ipv4.nat
   post-up /usr/local/tcp-server/kserverd -c /usr/local/tcp-server/kserver.conf
-  post-up /usr/local/tcp-server/kserver init_tasks --ip_on_leds
+  post-up /usr/local/tcp-server/kserver init_tasks --ip_on_leds 0x60000000
   pre-down iptables-restore < /etc/iptables.ipv4.nonat
   pre-down service isc-dhcp-server stop
   pre-down service hostapd stop
