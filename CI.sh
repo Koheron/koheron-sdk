609e2a3629b768a9e7d173d07a0d9e0c67b68c07
Jean Minet <jean.minet@gmail.com>
2016-02-03 22:30:43 +0100

    Update CI.sh

--- CI.sh
+++ CI.sh
@@ -4,5 +4,6 @@ source /opt/Xilinx/Vivado/2015.4/settings64.sh
 source /opt/Xilinx/SDK/2015.4/settings64.sh
 export XILINXD_LICENSE_FILE=/opt/Xilinx/Xilinx.lic
 make clean
-make NAME=oscillo zip
+make NAME=oscillo
 make NAME=spectrum zip
+
