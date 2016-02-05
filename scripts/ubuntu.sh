98334845b2f2f63986a2634986daf7407e22e78e
jeanminet <jean.minet@gmail.com>
2016-02-05 17:32:20 +0100

    clean rc.local

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -126,19 +126,18 @@ dpkg-reconfigure --frontend=noninteractive tzdata
 
 apt-get -y install openssh-server ca-certificates ntp usbutils psmisc lsof \
   parted curl less vim man-db iw wpasupplicant linux-firmware ntfs-3g gdb  \
-  bash-completion unzip
+  bash-completion unzip rsync
 
+apt-get install -y nginx
+apt-get install -y build-essential python-dev
 apt-get install -y python python-numpy
-
 apt-get install -y python-pip
+
 pip install koheron-tcp-client
 pip install flask
 pip install jinja2
 pip install urllib3
 pip install pyyaml
-
-apt-get install -y nginx
-apt-get install -y build-essential python-dev
 pip install uwsgi
 
 sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' etc/ssh/sshd_config
@@ -150,17 +149,9 @@ touch etc/udev/rules.d/75-persistent-net-generator.rules
 cat <<- EOF_CAT > etc/rc.local
 #!/bin/sh -e
 # rc.local
-exec 2> /var/log/rc.local.log  # send stderr from rc.local to a log file
-exec 1>&2                      # send stdout to the same log file
-set -x                         # tell sh to display commands before execution
-
-/usr/local/tcp-server/kserverd -c /usr/local/tcp-server/kserver.conf
-# chmod u+x /usr/local/tcp-server/relaunch_kserver.sh
-# nohup /usr/local/tcp-server/relaunch_kserver.sh 0<&- &>/dev/null &
-
-# Unzip app
-unzip -o /usr/local/flask/app.zip -d /usr/local/flask
-chmod u+x /usr/local/flask/deploy.sh
+#exec 2> /var/log/rc.local.log  # send stderr from rc.local to a log file
+#exec 1>&2                      # send stdout to the same log file
+#set -x                         # tell sh to display commands before execution
 exit 0
 EOF_CAT
 
@@ -177,11 +168,11 @@ iface eth0 inet dhcp
 #  netmask 255.255.255.0
 #  network 192.168.1.0
 #  broadcast 192.168.1.255
-
+  post-up unzip -o /usr/local/flask/app.zip -d /usr/local/flask
+  post-up /usr/local/tcp-server/kserverd -c /usr/local/tcp-server/kserver.conf
   post-up /usr/local/tcp-server/kserver init_tasks --ip_on_leds 0x60000000
   post-up ntpdate -u ntp.u-psud.fr
-  post-up bash /usr/local/flask/deploy.sh /usr/local/flask
-# post-up nohup /usr/local/flask/deploy.sh /usr/local/flask 0<&- &>/dev/null &
+  post-up bash /usr/local/flask/nginx.sh
 EOF_CAT
 
 cat <<- EOF_CAT > etc/network/interfaces.d/wlan0
@@ -192,9 +183,11 @@ iface wlan0 inet static
   post-up service hostapd restart
   post-up service isc-dhcp-server restart
   post-up iptables-restore < /etc/iptables.ipv4.nat
+  post-up unzip -o /usr/local/flask/app.zip -d /usr/local/flask
+  post-up /usr/local/tcp-server/kserverd -c /usr/local/tcp-server/kserver.conf
   post-up /usr/local/tcp-server/kserver init_tasks --ip_on_leds 0x60000000
-  post-up bash /usr/local/flask/deploy.sh /usr/local/flask
-# post-up nohup /usr/local/flask/deploy.sh /usr/local/flask 0<&- &>/dev/null &
+  post-up ntpdate -u ntp.u-psud.fr
+  post-up bash /usr/local/flask/nginx.sh
   pre-down iptables-restore < /etc/iptables.ipv4.nonat
   pre-down service isc-dhcp-server stop
   pre-down service hostapd stop
