2e53db0ea2611025a8340bc9f236fec05d4d0fab
jeanminet <jean.minet@gmail.com>
2016-02-23 17:21:36 +0100

    add dna_reader to base

--- projects/base/block_design.tcl
+++ projects/base/block_design.tcl
@@ -62,24 +62,26 @@ for {set i 0} {$i < 8} {incr i} {
 }
 
 cell pavel-demin:user:dna_reader:1.0 dna_0 {} {
-  aclk clk
+  aclk $adc_clk
   aresetn $rst_name/peripheral_aresetn
 }
 
-cell xilinx.com:ip:xlslice:1.0 dna_slice {
+cell xilinx.com:ip:xlslice:1.0 dna_slice_0 {
+  DIN_WIDTH 57
   DIN_FROM  31
   DIN_TO    0
 } {
   Din dna_0/dna_data
-  Dout $status_name/In9
+  Dout $status_name/In8
 }
 
-cell xilinx.com:ip:xlslice:1.0 dna_slice {
+cell xilinx.com:ip:xlslice:1.0 dna_slice_1 {
+  DIN_WIDTH 57
   DIN_FROM  56
   DIN_TO    32
 } {
   Din dna_0/dna_data
-  Dout $status_name/In10
+  Dout $status_name/In9
 }
 
 ##########################################################
