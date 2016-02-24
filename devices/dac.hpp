2f8b3270856a1e4443d1d656ad6f2bb2a2cbdaa3
thomas <th.vanderbruggen@gmail.com>
2016-02-24 18:09:53 +0100

    Split BASE into LASER and DAC

---
+++ devices/dac.hpp
@@ -0,0 +1,64 @@
+/// Fast DAC driver
+///
+/// (c) Koheron
+
+#ifndef __DRIVERS_DAC_HPP__
+#define __DRIVERS_DAC_HPP__
+
+#include <drivers/dev_mem.hpp>
+#include <drivers/wr_register.hpp>
+#include <drivers/addresses.hpp>
+
+class Dac
+{
+  public:
+    Dac(Klib::DevMem& dev_mem_);
+    ~Dac();
+
+    //> \io_type WRITE
+    //> \status ERROR_IF_NEG
+    //> \on_error Cannot open DAC device
+    //> \flag AT_INIT
+    int Open(uint32_t dac_wfm_size_);
+    
+    void Close();
+    
+    //> \description Reset to default state
+    //> \io_type WRITE
+    void reset();
+
+    //> \io_type WRITE_ARRAY param=>data param=>len
+    void set_dac_buffer(const uint32_t *data, uint32_t len);
+
+    // TODO (TV) Should be moved somewhere else ...
+    //> \io_type WRITE
+    void set_led(uint32_t value);
+
+    // XXX (TV) Is it for oscillo ??
+    //> \io_type WRITE
+    void reset_acquisition();
+
+    enum Status {
+        CLOSED,
+        OPENED,
+        FAILED
+    };
+
+    //> \is_failed
+    bool IsFailed() const {return status == FAILED;}
+
+  private:
+    // Core drivers
+    Klib::DevMem& dev_mem;
+        
+    int status;
+    
+    // Number of point in the DAC waveform
+    uint32_t dac_wfm_size;
+    
+    // Memory maps IDs:
+    Klib::MemMapID config_map;
+    Klib::MemMapID dac_map;
+};
+
+#endif // __DRIVERS_DAC_HPP__
---
+++ devices/laser.cpp
@@ -0,0 +1,108 @@
+/// (c) Koheron
+
+#include "laser.hpp"
+
+Laser::Laser(Klib::DevMem& dev_mem_)
+: dev_mem(dev_mem_),
+  xadc(dev_mem_),
+  gpio(dev_mem_)
+{
+    status = CLOSED;
+}
+
+Laser::~Laser()
+{
+    Close();
+}
+
+# define LASER_ENABLE_PIN 5
+
+int Laser::Open()
+{
+    if (status == CLOSED) {    
+        // Config is required for the PWMs
+        config_map = dev_mem.AddMemoryMap(CONFIG_ADDR, CONFIG_RANGE);
+        
+        if (static_cast<int>(config_map) < 0) {
+            status = FAILED;
+            return -1;
+        }
+        
+        // Open core drivers
+        xadc.Open();
+        gpio.Open();
+        
+        status = OPENED;
+        reset();
+    }
+    
+    return 0;
+}
+
+void Laser::Close()
+{
+    if (status == OPENED) {
+        reset();
+        dev_mem.RmMemoryMap(config_map);        
+        xadc.Close();
+        gpio.Close();
+        status = CLOSED;
+    }
+}
+
+void Laser::reset()
+{
+    assert(status == OPENED);
+
+    // XADC
+    xadc.set_channel(LASER_POWER_CHANNEL, LASER_CURRENT_CHANNEL);
+    xadc.enable_averaging();
+    xadc.set_averaging(256);
+
+    // GPIO
+    gpio.set_as_output(LASER_ENABLE_PIN, 2);
+    
+    stop_laser();
+    set_laser_current(0.0);
+}
+
+uint32_t Laser::get_laser_current()
+{
+    return xadc.read(LASER_CURRENT_CHANNEL);
+}
+
+uint32_t Laser::get_laser_power()
+{
+    return xadc.read(LASER_POWER_CHANNEL);
+}
+
+std::tuple<uint32_t, uint32_t> Laser::get_monitoring()
+{
+    return std::make_tuple(get_laser_current(), get_laser_power());
+}
+
+void Laser::start_laser()
+{
+    gpio.clear_bit(LASER_ENABLE_PIN, 2); // Laser enable on pin DIO7_P
+}
+
+void Laser::stop_laser()
+{
+    gpio.set_bit(LASER_ENABLE_PIN, 2); // Laser enable on pin DIO7_P
+}
+
+#define GAIN_LT1789 (1+200/10)
+#define PWM_MAX_VOLTAGE 1.8
+#define PWM_MAX_VALUE 1024
+#define MILLIAMPS_TO_AMPS 0.001
+#define CURRENT_TO_VOLTAGE(current) \
+    (current * MILLIAMPS_TO_AMPS * PWM_MAX_VALUE * GAIN_LT1789 / PWM_MAX_VOLTAGE)
+
+void Laser::set_laser_current(float current)
+{
+    float current_;
+    current > MAX_LASER_CURRENT ? current_ = MAX_LASER_CURRENT : current_ = current;    
+    uint32_t voltage = (uint32_t) CURRENT_TO_VOLTAGE(current_);
+    Klib::WriteReg32(dev_mem.GetBaseAddr(config_map) + PWM3_OFF, voltage);
+}
+
