9ec425f22fb73b6d8f00922fdda895380963d696
jeanminet <jean.minet@gmail.com>
2016-02-29 15:10:01 +0100

    update gpio.tcl

--- boards/red-pitaya/gpio.tcl
+++ boards/red-pitaya/gpio.tcl
@@ -9,8 +9,8 @@ proc add_gpio {{gpio_width 6} {idx "auto"} {intercon_idx 0}} {
     C_GPIO2_WIDTH $gpio_width
     C_IS_DUAL 1
   } {
-    s_axi_aclk /${::ps_name}/FCLK_CLK0
-    s_axi_aresetn /${::rst_name}/peripheral_aresetn
+    s_axi_aclk [set ::ps_clk$intercon_idx]
+    s_axi_aresetn [set ::rst${intercon_idx}_name]/peripheral_aresetn
   }
 
   connect_bd_intf_net -boundary_type upper [get_bd_intf_pins /axi_mem_intercon_$intercon_idx/M${idx}_AXI] [get_bd_intf_pins $gpio_name/S_AXI]
