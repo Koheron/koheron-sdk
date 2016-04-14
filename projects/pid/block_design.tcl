c355cea38d3bbd542a75f934f0f768e1ab964b04
jeanminet <jean.minet@gmail.com>
2016-04-14 15:11:41 +0200

    add digital down converter from red-pitaya-notes

--- projects/pid/block_design.tcl
+++ projects/pid/block_design.tcl
@@ -1,7 +1,7 @@
 source boards/$board_name/base_system.tcl
 
 # Connect DAC1 to config and ADC1 to status
-connect_pins $status_name/In$config::adc1_offset adc_dac/adc1
+#connect_pins $status_name/In$config::adc1_offset adc_dac/adc1
 
 # Add PID controller
 source lib/pid_controller.tcl
@@ -47,17 +47,11 @@ cell xilinx.com:ip:dds_compiler:6.0 $dds_name {
  DDS_Clock_Rate 125
  Parameter_Entry Hardware_Parameters
  Phase_Width 32
- Output_Width 14
+ Output_Width 24
  Phase_Increment Streaming
  Has_Phase_Out false
- Latency_Configuration Configurable
- Latency 2
- Noise_Shaping None
- Output_Width 14
- DATA_Has_TLAST Not_Required
- S_PHASE_Has_TUSER Not_Required
- M_DATA_Has_TUSER Not_Required
- Output_Frequency1 0
+ Noise_Shaping Taylor_Series_Corrected
+ Has_TREADY true
 } {
   aclk $adc_clk
 }
@@ -80,3 +74,174 @@ cell xilinx.com:ip:xlslice:1.0 slice_dac {
   Din $dds_name/m_axis_data_tdata
   Dout adc_dac/dac1
 }
+
+# Create axis_lfsr
+cell pavel-demin:user:axis_lfsr:1.0 lfsr_0 {} {
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create cmpy
+cell xilinx.com:ip:cmpy:6.0 mult_0 {
+  APORTWIDTH 14
+  BPORTWIDTH 24
+  ROUNDMODE Random_Rounding
+  OUTPUTWIDTH 25
+  LatencyConfig Manual
+  MinimumLatency 1
+} {
+  s_axis_a_tdata adc_dac/adc1
+  S_AXIS_B $dds_name/M_AXIS_DATA
+  S_AXIS_CTRL lfsr_0/M_AXIS
+  aclk $adc_clk
+}
+
+connect_constant mult_valid 1 1 mult_0/s_axis_a_tvalid
+
+# Create axis_broadcaster
+cell xilinx.com:ip:axis_broadcaster:1.1 bcast_0 {
+  S_TDATA_NUM_BYTES.VALUE_SRC USER
+  M_TDATA_NUM_BYTES.VALUE_SRC USER
+  S_TDATA_NUM_BYTES 8
+  M_TDATA_NUM_BYTES 3
+  M00_TDATA_REMAP {tdata[23:0]}
+  M01_TDATA_REMAP {tdata[55:32]}
+} {
+  S_AXIS mult_0/M_AXIS_DOUT
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create axis_variable
+cell pavel-demin:user:axis_variable:1.0 rate_0 {
+  AXIS_TDATA_WIDTH 16
+} {
+  cfg_data $config_name/Out$config::cic_rate_offset
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create axis_variable
+cell pavel-demin:user:axis_variable:1.0 rate_1 {
+  AXIS_TDATA_WIDTH 16
+} {
+  cfg_data $config_name/Out$config::cic_rate_offset
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create cic_compiler
+cell xilinx.com:ip:cic_compiler:4.0 cic_0 {
+  INPUT_DATA_WIDTH.VALUE_SRC USER
+  FILTER_TYPE Decimation
+  NUMBER_OF_STAGES 6
+  SAMPLE_RATE_CHANGES Programmable
+  MINIMUM_RATE 50
+  MAXIMUM_RATE 8192
+  FIXED_OR_INITIAL_RATE 625
+  INPUT_SAMPLE_FREQUENCY 125
+  CLOCK_FREQUENCY 125
+  INPUT_DATA_WIDTH 24
+  QUANTIZATION Truncation
+  OUTPUT_DATA_WIDTH 24
+  USE_XTREME_DSP_SLICE false
+  HAS_DOUT_TREADY true
+  HAS_ARESETN true
+} {
+  S_AXIS_DATA bcast_0/M00_AXIS
+  S_AXIS_CONFIG rate_0/M_AXIS
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create cic_compiler
+cell xilinx.com:ip:cic_compiler:4.0 cic_1 {
+  INPUT_DATA_WIDTH.VALUE_SRC USER
+  FILTER_TYPE Decimation
+  NUMBER_OF_STAGES 6
+  SAMPLE_RATE_CHANGES Programmable
+  MINIMUM_RATE 50
+  MAXIMUM_RATE 8192
+  FIXED_OR_INITIAL_RATE 625
+  INPUT_SAMPLE_FREQUENCY 125
+  CLOCK_FREQUENCY 125
+  INPUT_DATA_WIDTH 24
+  QUANTIZATION Truncation
+  OUTPUT_DATA_WIDTH 24
+  USE_XTREME_DSP_SLICE false
+  HAS_DOUT_TREADY true
+  HAS_ARESETN true
+} {
+  S_AXIS_DATA bcast_0/M01_AXIS
+  S_AXIS_CONFIG rate_1/M_AXIS
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create axis_combiner
+cell  xilinx.com:ip:axis_combiner:1.1 comb_0 {
+  TDATA_NUM_BYTES.VALUE_SRC USER
+  TDATA_NUM_BYTES 3
+} {
+  S00_AXIS cic_1/M_AXIS_DATA
+  S01_AXIS cic_0/M_AXIS_DATA
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create axis_dwidth_converter
+cell xilinx.com:ip:axis_dwidth_converter:1.1 conv_0 {
+  S_TDATA_NUM_BYTES.VALUE_SRC USER
+  S_TDATA_NUM_BYTES 6
+  M_TDATA_NUM_BYTES 3
+} {
+  S_AXIS comb_0/M_AXIS
+  aclk $adc_clk
+  aresetn $rst_adc_clk_name/peripheral_aresetn
+}
+
+# Create floating_point
+cell xilinx.com:ip:floating_point:7.1 fp_0 {
+  OPERATION_TYPE Fixed_to_float
+  A_PRECISION_TYPE.VALUE_SRC USER
+  C_A_EXPONENT_WIDTH.VALUE_SRC USER
+  C_A_FRACTION_WIDTH.VALUE_SRC USER
+  A_PRECISION_TYPE Custom
+  C_A_EXPONENT_WIDTH 2
+  C_A_FRACTION_WIDTH 22
+  RESULT_PRECISION_TYPE Single
+  Flow_Control NonBlocking
+  MaximumLatency False
+  C_Latency 2
+} {
+  S_AXIS_A conv_0/M_AXIS
+  aclk $adc_clk
+}
+
+set intercon_idx 1
+set idx 0
+cell xilinx.com:ip:axis_clock_converter:1.1 clock_converter {} {
+  S_AXIS fp_0/M_AXIS_RESULT
+  m_axis_aresetn [set rst${intercon_idx}_name]/peripheral_aresetn
+  s_axis_aclk $adc_clk
+  m_axis_aclk [set ps_clk$intercon_idx]
+}
+
+cell xilinx.com:ip:axi_fifo_mm_s:4.1 axis_fifo {
+  C_USE_TX_DATA 0
+  C_USE_TX_CTRL 0
+  C_USE_RX_CUT_THROUGH true
+  C_RX_FIFO_DEPTH 8192
+  C_RX_FIFO_PF_THRESHOLD 4096
+} {
+  s_axi_aclk [set ps_clk$intercon_idx]
+  s_axi_aresetn [set rst${intercon_idx}_name]/peripheral_aresetn
+  S_AXI [set interconnect_${intercon_idx}_name]/M${idx}_AXI
+  AXI_STR_RXD clock_converter/M_AXIS
+}
+
+assign_bd_address [get_bd_addr_segs axis_fifo/S_AXI/Mem0]
+set memory_segment [get_bd_addr_segs /${::ps_name}/Data/SEG_axis_fifo_Mem0]
+set_property offset $config::axi_fifo_offset $memory_segment
+set_property range $config::axi_fifo_range $memory_segment
+
