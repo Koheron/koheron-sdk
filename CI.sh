56796eb7ea6c59bdb0eec6d253d9d0c5753e7000
jeanminet <jeanminet>
2016-04-22 14:28:34 +0200

    edit CI.sh

--- CI.sh
+++ CI.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-source /opt/Xilinx/Vivado/2015.4/settings64.sh
-source /opt/Xilinx/SDK/2015.4/settings64.sh
+. settings.sh
 export XILINXD_LICENSE_FILE=/opt/Xilinx/Xilinx.lic
 
 make NAME=oscillo
