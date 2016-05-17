72929f34c647526f80ddb31501cafc0ee8667752
jeanminet <jean.minet@gmail.com>
2016-05-17 16:37:38 +0200

    edit peak_detector test bench

--- projects/peak_detector_test/test_bench.v
+++ projects/peak_detector_test/test_bench.v
@@ -2,16 +2,16 @@
 
 module peak_detector_tb();
 
-  parameter WFM_SIZE = 8;
+  parameter WFM_WIDTH = 8;
   
   reg clk;
   reg [31:0] din;
-  reg [WFM_SIZE-1:0] address_low;
-  reg [WFM_SIZE-1:0] address_high;
-  reg [WFM_SIZE-1:0] address_reset;
+  reg [WFM_WIDTH-1:0] address_low;
+  reg [WFM_WIDTH-1:0] address_high;
+  reg [WFM_WIDTH-1:0] address_reset;
   reg s_axis_tvalid;
 
-  wire [WFM_SIZE-1:0] address_out;
+  wire [WFM_WIDTH-1:0] address_out;
   wire [31:0] maximum_out;
   wire m_axis_tvalid;
 
@@ -34,11 +34,10 @@ module peak_detector_tb();
     clk = 0;
     din = 0;
     address_low = 0;
-    address_high = 2**WFM_SIZE-1;
-    address_reset = (address_low+WFM_SIZE-1) % WFM_SIZE;
+    address_high = 2**WFM_WIDTH-1;
+    address_reset = (address_low+2**WFM_WIDTH - 1) % (2**WFM_WIDTH);
     s_axis_tvalid = 0;
-    #(2**WFM_SIZE * CLK_PERIOD) s_axis_tvalid = 1;
-    #(10 * CLK_PERIOD) din = 1;
+    #(2**WFM_WIDTH * CLK_PERIOD) s_axis_tvalid = 1;
     #(10000 * CLK_PERIOD)
     $finish;
   end
@@ -46,11 +45,11 @@ module peak_detector_tb();
   always #(CLK_PERIOD/2) clk = ~clk;
 
   always begin
-    #(CLK_PERIOD * 2**(WFM_SIZE-2)) din = 0;
-    #(CLK_PERIOD * 2**(WFM_SIZE-2)) din = 1;
-    #(CLK_PERIOD * 2**(WFM_SIZE-2)) din = 2;
-    #(CLK_PERIOD * 2**(WFM_SIZE-2)) din = 3;
-  end   
+    #(CLK_PERIOD * 2**(WFM_WIDTH-2)) din = 1;
+    #(CLK_PERIOD * 2**(WFM_WIDTH-2)) din = 2;
+    #(CLK_PERIOD * 2**(WFM_WIDTH-2)) din = 3;
+    #(CLK_PERIOD * 2**(WFM_WIDTH-2)) din = 0;
+  end
   
 endmodule
 
