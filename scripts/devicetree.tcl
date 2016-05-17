5055ae30cc1342c1401a599d5031203cfc1891ac
jeanminet <jean.minet@gmail.com>
2016-05-17 11:42:48 +0200

    add parameter $vivado_version in device_tree.tcl

--- scripts/devicetree.tcl
+++ scripts/devicetree.tcl
@@ -5,6 +5,8 @@ set proc_name [lindex $argv 1]
 
 set repo_path [lindex $argv 2]
 
+set vivado_version [lindex $argv 3]
+
 set boot_args {console=ttyPS0,115200 root=/dev/mmcblk0p2 ro rootfstype=ext4 earlyprintk rootwait}
 
 set hard_path tmp/$project_name.hard
@@ -18,7 +20,7 @@ set_repo_path $repo_path
 open_hw_design $hard_path/$project_name.hdf
 create_sw_design -proc $proc_name -os device_tree devicetree
 
-set_property CONFIG.kernel_version {2016.1} [get_os]
+set_property CONFIG.kernel_version {$vivado_version} [get_os]
 set_property CONFIG.bootargs $boot_args [get_os]
 
 generate_bsp -dir $tree_path
