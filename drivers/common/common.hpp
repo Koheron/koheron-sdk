cdde734142d635958edd7faa2270a0553fcf7c47
koheron <th.vanderbruggen@gmail.com>
2016-06-05 00:43:34 +0200

    Update gpio and common

--- drivers/common/common.hpp
+++ drivers/common/common.hpp
@@ -14,21 +14,21 @@
 class Common
 {
   public:
-    Common(Klib::DevMem& dvm_);
+    Common(Klib::DevMem& dvm_)
+    : dvm(dvm_)
+    {
+        config_map = dvm.AddMemoryMap(CONFIG_ADDR, CONFIG_RANGE);
+        status_map = dvm.AddMemoryMap(STATUS_ADDR, STATUS_RANGE, Klib::MemoryMap::READ_ONLY);
+    }
 
-    int Open();
+    int Open() {return dvm.is_ok() ? 0 : -1;}
 
     std::array<uint32_t, BITSTREAM_ID_SIZE> get_bitstream_id();
 
     uint64_t get_dna();
 
-    void set_led(uint32_t value) {
-        dvm.write32(config_map, LED_OFF, value);
-    }
-
-    uint32_t get_led() {
-        return dvm.read32(config_map, LED_OFF);
-    }
+    void set_led(uint32_t value) {dvm.write32(config_map, LED_OFF, value);}
+    uint32_t get_led()           {return dvm.read32(config_map, LED_OFF);}
 
     void ip_on_leds();
 
@@ -38,23 +38,16 @@ class Common
         init.load_settings();
     };
 
-    enum Status {
-        CLOSED,
-        OPENED,
-        FAILED
-    };
-
     #pragma tcp-server is_failed
-    bool IsFailed() const {return status == FAILED;}
+    bool IsFailed() const {return dvm.IsFailed();}
 
   private:
     Klib::DevMem& dvm;
-    int status;
-    std::array<uint32_t, BITSTREAM_ID_SIZE> bitstream_id;
 
-    // Memory maps IDs
     Klib::MemMapID config_map;
     Klib::MemMapID status_map;
+
+    std::array<uint32_t, BITSTREAM_ID_SIZE> bitstream_id;
 };
 
 #endif // __DRIVERS_COMMON_HPP__
