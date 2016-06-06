9859020da59e831f7d20e2f61f3fade1c0a5d416
jeanminet <jean.minet@gmail.com>
2016-06-06 12:04:35 +0200

    fix write_buffer in device_memory

--- drivers/device_memory/__init__.py
+++ drivers/device_memory/__init__.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import re
-from koheron_tcp_client import command, write_buffer
+import koheron_tcp_client as kc
 
 class DeviceMemory(object):
 
@@ -11,7 +11,7 @@ class DeviceMemory(object):
         self.maps = {} 
 
     def add_mmap(self, mmap):
-        @command('DEVICE_MEMORY','II')
+        @kc.command('DEVICE_MEMORY','II')
         def add_memory_map(self, device_addr, map_size):
             return self.client.recv_int32()
         if not mmap.name in self.maps:
@@ -22,37 +22,37 @@ class DeviceMemory(object):
                 self.maps[mmap.name] = mmap_idx
 
     def read32(self, device_name, offset):
-        @command('DEVICE_MEMORY','II')
+        @kc.command('DEVICE_MEMORY','II')
         def read(self, mmap_idx, offset):
             return self.client.recv_uint32()
         return read(self, self.maps[device_name], offset)
 
     def write32(self, device_name, offset, value):
-        @command('DEVICE_MEMORY','III')
+        @kc.command('DEVICE_MEMORY','III')
         def write(self, mmap_idx, offset, value):
             pass
         write(self, self.maps[device_name], offset, value)
 
     def write_buffer(self, device_name, offset, data, format_char='I', dtype=np.uint32):
-        @write_buffer('DEVICE_MEMORY', format_char=format_char, dtype=dtype)
+        @kc.write_buffer('DEVICE_MEMORY', 'II', format_char=format_char, dtype=dtype)
         def write_buffer(self, data, mmap_idx, offset): 
             pass
         write_buffer(self, data, self.maps[device_name], offset)
 
     def set_bit(self, device_name, offset, index):
-        @command('DEVICE_MEMORY', 'III')
+        @kc.command('DEVICE_MEMORY', 'III')
         def set_bit(self, mmap_idx, offset, index):
             pass
         set_bit(self, self.maps[device_name], offset, index)
 
     def clear_bit(self, device_name, offset, index):
-        @command('DEVICE_MEMORY', 'III')
+        @kc.command('DEVICE_MEMORY', 'III')
         def clear_bit(self, mmap_idx, offset, index):
             pass
         clear_bit(self, self.maps[device_name], offset, index)
 
     def toggle_bit(self, device_name, offset, index):
-        @command('DEVICE_MEMORY', 'III')
+        @kc.command('DEVICE_MEMORY', 'III')
         def toggle_bit(self, mmap_idx, offset, index):
             pass
         toggle_bit(self, self.maps[device_name], offset, index)
