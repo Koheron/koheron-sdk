537da3a0acd80006ae3959c787145db8d96cc89f
jeanminet <jean.minet@gmail.com>
2016-06-07 17:32:53 +0200

    minor edit project.tcl

--- scripts/project.tcl
+++ scripts/project.tcl
@@ -6,7 +6,7 @@ set part_name [lindex $argv 1]
 set board_name [lindex $argv 2]
 
 set cfg boards/$board_name/config
-
+set lib fpga/tcl
 
 file delete -force tmp/$project_name.cache tmp/$project_name.hw tmp/$project_name.srcs tmp/$project_name.runs tmp/$project_name.xpr tmp/$project_name.sim
 
@@ -19,7 +19,6 @@ set bd_path tmp/$project_name.srcs/sources_1/bd/system
 create_bd_design system
 
 source $cfg/ports.tcl
-set lib fpga/tcl
 source $lib/utilities.tcl
 
 source projects/$project_name/block_design.tcl
