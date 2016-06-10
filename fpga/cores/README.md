b8abc3e3c8f235220cc0958739e3453f8e1ea5e7
Jean Minet <jean.minet@gmail.com>
2016-06-10 17:59:43 +0200

    Add latched_mux core (#145)
    * start simple interconnect core
    
    * use verilog part-select +:
    
    * remove clogb2 (TODO automatic determination of SEL_WIDTH parameter)
    
    * add clk enable to simple_interconnect
    
    * add fpga/cores/README.md
    
    * rename simple_interconnect to latched_mux

---
+++ fpga/cores/README.md
@@ -0,0 +1,7 @@
+# Cores
+
+Cores are written in Verilog HDL and can be tested using commands similar to:
+
+```
+make CORE=address_generator_v1_0 test_core
+```
---
+++ fpga/cores/latched_mux_v1_0/latched_mux.v
@@ -0,0 +1,33 @@
+`timescale 1 ns / 1 ps
+
+module latched_mux #
+(
+  parameter integer WIDTH = 32,
+  parameter integer N_INPUTS = 3,
+  parameter integer SEL_WIDTH = 2
+)
+(
+  input  wire                          clk,
+  input  wire                          clken,
+  input  wire [(N_INPUTS * WIDTH)-1:0] in,
+  input  wire [SEL_WIDTH-1 :0]         sel,
+  output reg  [WIDTH-1:0]              out
+);
+
+  reg [SEL_WIDTH-1 :0] sel_reg;
+  initial sel_reg = {(SEL_WIDTH){1'b0}};
+
+  always @(posedge clk) begin
+    if (clken == 1'b1) begin
+       sel_reg <= sel;
+    end
+  end
+
+  // http://stackoverflow.com/questions/25123924/verilog-range-must-be-bounded-by-constant-expression
+  always @(posedge clk) begin
+    out <= in[sel_reg * WIDTH +: WIDTH];
+  end
+
+endmodule
+
+
