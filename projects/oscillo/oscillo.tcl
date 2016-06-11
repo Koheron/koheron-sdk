886e70b612752c40732217ad8480ef68f60e5335
jeanminet <jean.minet@gmail.com>
2016-06-11 22:00:47 +0200

    add connect_cell procedure

--- projects/oscillo/oscillo.tcl
+++ projects/oscillo/oscillo.tcl
@@ -19,19 +19,20 @@ for {set i 0} {$i < 2} {incr i} {
   # Add averaging module
   add_averager_module $avg_name $config::bram_addr_width -input_type fix_$config::adc_width
 
-  connect_pins $avg_name/clk         $adc_clk
-  connect_pins $avg_name/restart     $address_name/restart
-
-  connect_pins $avg_name/avg_on      [cfg_pin avg$i]
-  connect_pins $avg_name/period      [cfg_pin period$i]
-  connect_pins $avg_name/threshold   [cfg_pin threshold$i]
-  connect_pins $avg_name/n_avg_min   [cfg_pin n_avg_min$i]
-  connect_pins $avg_name/tvalid      $address_name/tvalid
-  connect_pins $avg_name/din         adc_dac/adc[expr $i + 1]
-  connect_pins $avg_name/addr        $adc_recorder_name/addr
-  connect_pins $avg_name/dout        $adc_recorder_name/adc
-  connect_pins $avg_name/wen         $adc_recorder_name/wen
-  connect_pins $avg_name/n_avg       [sts_pin n_avg$i]
-  connect_pins $avg_name/ready       [sts_pin avg_ready$i]
-  connect_pins $avg_name/avg_on_out  [sts_pin avg_on_out$i]
+  connect_cell $avg_name {
+    clk         $adc_clk
+    restart     $address_name/restart
+    avg_on      [cfg_pin avg$i]
+    period      [cfg_pin period$i]
+    threshold   [cfg_pin threshold$i]
+    n_avg_min   [cfg_pin n_avg_min$i]
+    tvalid      $address_name/tvalid
+    din         adc_dac/adc[expr $i + 1]
+    addr        $adc_recorder_name/addr
+    dout        $adc_recorder_name/adc
+    wen         $adc_recorder_name/wen
+    n_avg       [sts_pin n_avg$i]
+    ready       [sts_pin avg_ready$i]
+    avg_on_out  [sts_pin avg_on_out$i]
+  }
 }
