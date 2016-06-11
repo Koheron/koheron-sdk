9961cf02c04f7b15e7512932e06aa7b7c5f0e7f5
jeanminet <jean.minet@gmail.com>
2016-06-11 21:22:33 +0200

    rename adc_contrller to bram_recorder and update spectrum instrument

--- fpga/tcl/bram_recorder.tcl
+++ fpga/tcl/bram_recorder.tcl
@@ -1,20 +1,18 @@
 source $lib/bram.tcl
 
-# Single ADC controller (32 bit width)
+# Single BRAM recorder (32 bit width)
 
-proc add_adc_controller {module_name bram_name} {
+proc add_bram_recorder {module_name bram_name} {
 
   set bd [current_bd_instance .]
   current_bd_instance [create_bd_cell -type hier $module_name]
 
   create_bd_pin -dir I -from [expr $config::bram_addr_width + 1] -to 0 addr
+  create_bd_pin -dir I -from 31 -to 0 adc
+  create_bd_pin -dir I -from 3  -to 0 wen
   create_bd_pin -dir I -type clk clk
   create_bd_pin -dir I rst
 
-  create_bd_pin -dir I -from 31 -to 0 adc
-  create_bd_pin -dir I -from 3  -to 0 wen
-
-  set bram_name adc_bram
   add_bram $bram_name [set config::axi_${bram_name}_range] [set config::axi_${bram_name}_offset]
 
   connect_constant ${bram_name}_enb  1 1  blk_mem_gen_$bram_name/enb
--- projects/oscillo/oscillo.tcl
+++ projects/oscillo/oscillo.tcl
@@ -1,8 +1,8 @@
 source projects/averager_module/averager.tcl
-source $lib/adc_controller.tcl
+source $lib/bram_recorder.tcl
 
 ###########################################################
-# Add ADC BRAMs
+# Add ADC BRAM recorders
 ###########################################################
 
 for {set i 0} {$i < 2} {incr i} {
@@ -10,11 +10,11 @@ for {set i 0} {$i < 2} {incr i} {
   set adc_bram_name adc${i}_bram
   set avg_name      avg$i
 
-  set adc_controller_name adc_ctrl$i 
-  add_adc_controller $adc_controller_name adc[expr {$i + 1}]
+  set adc_recorder_name adc_recorder$i 
+  add_bram_recorder $adc_recorder_name adc[expr {$i + 1}]
 
-  connect_pins $adc_controller_name/clk   $adc_clk
-  connect_pins $adc_controller_name/rst   $rst_adc_clk_name/peripheral_reset
+  connect_pins $adc_recorder_name/clk   $adc_clk
+  connect_pins $adc_recorder_name/rst   $rst_adc_clk_name/peripheral_reset
 
   # Add averaging module
   add_averager_module $avg_name $config::bram_addr_width -input_type fix_$config::adc_width
@@ -28,9 +28,9 @@ for {set i 0} {$i < 2} {incr i} {
   connect_pins $avg_name/n_avg_min   [cfg_pin n_avg_min$i]
   connect_pins $avg_name/tvalid      $address_name/tvalid
   connect_pins $avg_name/din         adc_dac/adc[expr $i + 1]
-  connect_pins $avg_name/addr        $adc_controller_name/addr
-  connect_pins $avg_name/dout        $adc_controller_name/adc
-  connect_pins $avg_name/wen         $adc_controller_name/wen
+  connect_pins $avg_name/addr        $adc_recorder_name/addr
+  connect_pins $avg_name/dout        $adc_recorder_name/adc
+  connect_pins $avg_name/wen         $adc_recorder_name/wen
   connect_pins $avg_name/n_avg       [sts_pin n_avg$i]
   connect_pins $avg_name/ready       [sts_pin avg_ready$i]
   connect_pins $avg_name/avg_on_out  [sts_pin avg_on_out$i]
