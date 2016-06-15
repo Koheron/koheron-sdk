530f2f59e44e37e65e5dfcd8a3bf8eb142699284
koheron <th.vanderbruggen@gmail.com>
2016-06-15 16:38:07 +0200

    Add test app build

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 make DOCKER=True NAME=oscillo tmp/oscillo.tcp-server/tmp/kserverd
 make DOCKER=True NAME=spectrum tmp/spectrum.tcp-server/tmp/kserverd
 make DOCKER=True NAME=pid tmp/pid.tcp-server/tmp/kserverd
+
+make DOCKER=True app
