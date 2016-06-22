5457df52f3dcf6504833d11542222707b1665a63
jeanminet <jean.minet@gmail.com>
2016-06-22 15:32:54 +0200

    use namespaces for modules

--- projects/address_module/block_design.tcl
+++ projects/address_module/block_design.tcl
@@ -2,14 +2,7 @@ source projects/$project_name/config.tcl
 source projects/$project_name/address.tcl
 
 set module address
-add_address_module $module $config::bram_addr_width
 
-create_bd_port -dir I -type clk clk
-create_bd_port -dir I -from 31 -to 0 cfg
-create_bd_port -dir I -from 31 -to 0 period
-
-create_bd_port -dir O -from [expr $config::bram_addr_width+1] -to 0 addr
-create_bd_port -dir O restart
-create_bd_port -dir O tvalid
-
+address::create $module $config::bram_addr_width
+address::pins create_bd_port $config::bram_addr_width
 connect_ports $module
