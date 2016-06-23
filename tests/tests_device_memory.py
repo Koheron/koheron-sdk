7e10167ec5712b4f5ab786e5a0cf54b1848d5e46
thomas <th.vanderbruggen@gmail.com>
2016-06-23 15:33:30 +0200

    Add test_write_read_buffer

--- tests/tests_device_memory.py
+++ tests/tests_device_memory.py
@@ -1,5 +1,6 @@
 import context
 import os
+import numpy as np
 
 from instrument_manager import InstrumentManager
 from koheron_tcp_client import KClient
@@ -21,10 +22,17 @@ for mmap in pc.mmaps:
     dvm.add_mmap(mmap)
 
 class TestsDeviceMemory:
-    def test_write32(self):
+    def test_write_read(self):
         value = 42
         dvm.write32('config', pc.cfg['led'], value)
         assert(dvm.read32('config', pc.cfg['led']) == value)
 
-# tests = TestsDeviceMemory()
-# tests.test_write32()
+    def test_write_read_buffer(self):
+        buff = np.arange(10)
+        dvm.write_buffer('dac', 0, buff)
+        buff_ret = dvm.read_buffer('dac', 0, len(buff))
+        assert np.array_equal(buff, buff_ret)
+
+tests = TestsDeviceMemory()
+# tests.test_write_read()
+# tests.test_write_read_buffer()
