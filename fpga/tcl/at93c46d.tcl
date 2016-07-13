d8aa2fe14e382c839e95a095c43afac57e5de997
jeanminet <jean.minet@gmail.com>
2016-07-13 14:12:16 +0200

    clean eeprom

--- fpga/tcl/at93c46d.tcl
+++ fpga/tcl/at93c46d.tcl
@@ -6,8 +6,11 @@ create_bd_port -dir O spi_sclk
 create_bd_port -dir O spi_cs
 
 cell koheron:user:at93c46d_spi:1.0 at93c46d_spi_0 {} {
-  clk $adc_clk
+  clk      $adc_clk
   data_out [sts_pin spi_out]
+  start    [get_slice_pin [cfg_pin spi_in] 0 0]
+  cmd      [get_slice_pin [cfg_pin spi_in] 8 1]
+  data_in  [get_slice_pin [cfg_pin spi_in] 31 16]
 }
 
 foreach {name} {
@@ -17,25 +20,4 @@ foreach {name} {
   cs
 } {
   connect_bd_net [get_bd_ports spi_$name] [get_bd_pins at93c46d_spi_0/$name]
-}
-
-cell xilinx.com:ip:xlslice:1.0 slice_start_eeprom {} {
-  Dout at93c46d_spi_0/start
-  Din [cfg_pin spi_in]
-}
-
-cell xilinx.com:ip:xlslice:1.0 slice_cmd_eeprom {
-  DIN_TO 1
-  DIN_FROM 8
-} {
-  Dout at93c46d_spi_0/cmd
-  Din [cfg_pin spi_in]
-}
-
-cell xilinx.com:ip:xlslice:1.0 slice_data_in_eeprom {
-  DIN_TO 16
-  DIN_FROM 31
-} {
-  Dout at93c46d_spi_0/data_in
-  Din [cfg_pin spi_in]
 }
