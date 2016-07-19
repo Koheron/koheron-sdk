a08b1ddfef5a0715fdbeab408d9fa0cf07599d81
thomas <th.vanderbruggen@gmail.com>
2016-07-19 13:07:49 +0200

    Add test laser current

--- tests/tests_laser.py
+++ tests/tests_laser.py
@@ -12,7 +12,7 @@ host = os.getenv('HOST','192.168.1.2')
 project = os.getenv('NAME', '')
 
 im = InstrumentManager(host)
-im.install_instrument(project, always_restart=True)
+im.install_instrument(project, always_restart=False)
 
 client = KClient(host)
 laser = Laser(client)
@@ -21,7 +21,14 @@ class TestsLaser:
     def test_is_laser_present(self):
         assert laser.is_laser_present()
 
-    def test_laser_on(self):
+    def test_laser_current(self):
+        laser.start_laser()
+        curr_setpt = 26.3 # mA
+        laser.set_laser_current(curr_setpt)
+        rel_err = abs(curr_setpt * 1E-3 / laser.get_laser_current() - 1)
+        assert rel_err < 0.1
+
+    def test_laser_power(self):
         laser.start_laser()
         laser.set_laser_current(0.0)
         time.sleep(0.1)
