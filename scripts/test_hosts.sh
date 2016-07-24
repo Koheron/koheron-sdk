c8a607429d5658f15ab7309661832771218a60ee
thomas <th.vanderbruggen@gmail.com>
2016-07-24 21:33:59 +0200

    Test instrument manager

--- scripts/test_hosts.sh
+++ scripts/test_hosts.sh
@@ -4,6 +4,9 @@ set -e
 
 # Lase board
 # 192.168.1.5
+make HOST=192.168.1.5 app_sync
+sleep 3
+make NAME=oscillo HOST=192.168.1.5 test_instrument_manager
 make NAME=oscillo HOST=192.168.1.5 test_instrum
 make NAME=oscillo HOST=192.168.1.5 test_laser
 make NAME=oscillo HOST=192.168.1.5 test_eeprom
