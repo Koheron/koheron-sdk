60e9a4a5e60599c05f35cc21244fa4ac9048d8b7
jeanminet <jean.minet@gmail.com>
2016-08-10 18:14:51 +0200

    use jinja to fill start.sh

--- scripts/templates/start.sh
+++ scripts/templates/start.sh
@@ -5,10 +5,19 @@ echo 'Clock initialization...'
 
 devcfg=/sys/devices/soc0/amba/f8007000.devcfg
 
-clk0 = 200000000
-test -d $devcfg/fclk/fclk0 || echo fclk0 > $devcfg/fclk_export
-echo 1 > $devcfg/fclk/fclk0/enable
-echo $clk0 > $devcfg/fclk/fclk0/set_rate
+clk0 = {{ dic['parameters']['clk0'] }}
+clk1 = {{ dic['parameters']['clk1'] }}
+
+function set_fclk () {
+    fclk_name = $1
+    fclk_val = $2
+    test -d $devcfg/fclk/$fclk || echo $fclk > $devcfg/fclk_export
+    echo 1 > $devcfg/fclk/$fclk/enable
+    echo $fclk_val > $devcfg/fclk/$fclk/set_rate
+}
+
+set_fclk fclk0 $clk0
+set_fclk fclk1 $clk1
 
 echo 'Load bitstream'
 /bin/cat /tmp/instrument/${CURRENT_INSTRUMENT}.bit > /dev/xdevcfg
