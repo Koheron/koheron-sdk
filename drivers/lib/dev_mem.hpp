fb1a51df1b0a7f68d2c2c3c37519be5ed2ba6291
thomas <th.vanderbruggen@gmail.com>
2016-08-19 18:15:47 +0200

    Remove kserver

--- drivers/lib/dev_mem.hpp
+++ drivers/lib/dev_mem.hpp
@@ -20,8 +20,6 @@ extern "C" {
     #include <sys/mman.h>
 }
 
-namespace kserver {class KServer;}
-
 #include "memory_map.hpp"
 
 template<size_t N, class = std::make_index_sequence<N>>
@@ -31,7 +29,10 @@ template<size_t N, MemMapID... ids>
 class DevMemImpl<N, std::index_sequence<ids...>>
 {
   public:
-    DevMemImpl(kserver::KServer *kserver_);
+    DevMemImpl()
+    : fd(-1)
+    , failed_maps(0)
+    {}
 
     ~DevMemImpl() {close(fd);}
 
@@ -43,14 +44,11 @@ class DevMemImpl<N, std::index_sequence<ids...>>
     }
 
   private:
-    kserver::KServer *kserver;
-    int fd;         // /dev/mem file ID
+    int fd; // /dev/mem file ID
+    std::vector<MemMapID> failed_maps;
+    std::tuple<MemoryMap<ids>...> mem_maps;
 
     template<MemMapID id> void add_memory_map();
-
-    std::vector<MemMapID> failed_maps;
-
-    std::tuple<MemoryMap<ids>...> mem_maps;
 
     template<MemMapID cnt>
     std::enable_if_t<cnt == 0, void>
@@ -63,14 +61,6 @@ class DevMemImpl<N, std::index_sequence<ids...>>
         create_maps<cnt-1>();
     }
 };
-
-template<size_t N, MemMapID... ids>
-DevMemImpl<N, std::index_sequence<ids...>>::DevMemImpl(kserver::KServer *kserver_)
-: kserver(kserver_)
-, failed_maps(0)
-{
-    fd = -1;
-}
 
 template<size_t N, MemMapID... ids>
 int DevMemImpl<N, std::index_sequence<ids...>>::open()
