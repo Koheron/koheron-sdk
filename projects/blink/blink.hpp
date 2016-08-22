41e5fe29a9cf0c9c8087c9662687451d05d74caa
jeanminet <jean.minet@gmail.com>
2016-08-22 20:46:26 +0200

    blink server ok

--- projects/blink/blink.hpp
+++ projects/blink/blink.hpp
@@ -5,30 +5,30 @@
 #ifndef __DRIVERS_BLINK_HPP__
 #define __DRIVERS_BLINK_HPP__
 
-#include <drivers/lib/dev_mem.hpp>
-#include <drivers/addresses.hpp>
+#include <drivers/lib/memory_manager.hpp>
+#include <drivers/memory.hpp>
 
 class Blink
 {
   public:
-    Blink(DevMem& dvm)
-    : cfg(dvm.get<CONFIG_MEM>())
-    , sts(dvm.get<STATUS_MEM>())
+    Blink(MemoryManager& mm)
+    : cfg(mm.get<mem::config>())
+    , sts(mm.get<mem::status>())
     {}
 
     void set_dac(uint32_t dac0, uint32_t dac1) {
-        cfg.write<DAC0_OFF>(dac0);
-        cfg.write<DAC1_OFF>(dac1);
+        cfg.write<reg::dac0>(dac0);
+        cfg.write<reg::dac1>(dac1);
     }
 
     std::tuple<uint32_t, uint32_t>
     get_adc() {
-        return std::make_tuple(sts.read<ADC0_OFF>(), sts.read<ADC1_OFF>());
+        return std::make_tuple(sts.read<reg::adc0>(), sts.read<reg::adc1>());
     }
 
   private:
-    MemoryMap<CONFIG_MEM>& cfg;
-    MemoryMap<STATUS_MEM>& sts;
+    MemoryMap<mem::config>& cfg;
+    MemoryMap<mem::status>& sts;
 
 };
 
