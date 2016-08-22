68c61ebd8ecd7bc3036991da930d4aca18bc099d
jeanminet <jean.minet@gmail.com>
2016-08-22 20:23:02 +0200

    update spectrum

--- drivers/spectrum/spectrum.hpp
+++ drivers/spectrum/spectrum.hpp
@@ -2,8 +2,8 @@
 ///
 /// (c) Koheron
 
-#ifndef __DRIVERS_CORE_SPECTRUM_HPP__
-#define __DRIVERS_CORE_SPECTRUM_HPP__
+#ifndef __DRIVERS_SPECTRUM_HPP__
+#define __DRIVERS_SPECTRUM_HPP__
 
 #include <drivers/lib/fifo_reader.hpp>
 #include <drivers/lib/dac_router.hpp>
@@ -19,13 +19,13 @@ class Spectrum
     Spectrum(DevMem& dvm);
 
     void reset() {
-        cfg.clear_bit<ADDR_OFF, 0>();
-        cfg.set_bit<ADDR_OFF, 0>();
+        cfg.clear_bit<reg::addr, 0>();
+        cfg.set_bit<reg::addr, 0>();
     }
 
     void set_n_avg_min(uint32_t n_avg_min) {
         uint32_t n_avg_min_ = (n_avg_min < 2) ? 0 : n_avg_min-2;
-        cfg.write<N_AVG_MIN_OFF>(n_avg_min_);
+        cfg.write<reg::n_avg_min>(n_avg_min_);
     }
 
     void set_period(uint32_t period) {
@@ -35,13 +35,13 @@ class Spectrum
     }
 
     void set_dac_period(uint32_t dac_period0, uint32_t dac_period1) {
-        cfg.write<DAC_PERIOD0_OFF>(dac_period0 - 1);
-        cfg.write<DAC_PERIOD1_OFF>(dac_period1 - 1);
+        cfg.write<reg::dac_period0>(dac_period0 - 1);
+        cfg.write<reg::dac_period1>(dac_period1 - 1);
     }
 
     void set_avg_period(uint32_t avg_period) {
-        cfg.write<AVG_PERIOD_OFF>(avg_period - 1);
-        cfg.write<AVG_THRESHOLD_OFF>(avg_period - 6);
+        cfg.write<reg::avg_period>(avg_period - 1);
+        cfg.write<reg::avg_threshold>(avg_period - 6);
     }
 
     void set_dac_buffer(uint32_t channel, const std::array<uint32_t, WFM_SIZE/2>& arr) {
@@ -53,17 +53,17 @@ class Spectrum
     }
 
     void reset_acquisition() {
-        cfg.clear_bit<ADDR_OFF, 1>();
-        cfg.set_bit<ADDR_OFF, 1>();
+        cfg.clear_bit<reg::addr, 1>();
+        cfg.set_bit<reg::addr, 1>();
     }
 
     void set_scale_sch(uint32_t scale_sch) {
         // LSB at 1 for forward FFT
-        cfg.write<CFG_FFT_OFF>(1 + 2 * scale_sch);
+        cfg.write<reg::cfg_fft>(1 + 2 * scale_sch);
     }
 
     void set_offset(uint32_t offset_real, uint32_t offset_imag) {
-        cfg.write<SUBSTRACT_MEAN_OFF>(offset_real + 16384 * offset_imag);
+        cfg.write<reg::substract_mean>(offset_real + 16384 * offset_imag);
     }
 
     void set_demod_buffer(const std::array<uint32_t, WFM_SIZE>& arr) {
@@ -80,13 +80,13 @@ class Spectrum
 
     void set_averaging(bool avg_status);
 
-    uint32_t get_num_average()  {return sts.read<N_AVG_OFF>();}
-    uint32_t get_peak_address() {return sts.read<PEAK_ADDRESS_OFF>();}
-    uint32_t get_peak_maximum() {return sts.read<PEAK_MAXIMUM_OFF>();}
+    uint32_t get_num_average()  {return sts.read<reg::n_avg>();}
+    uint32_t get_peak_address() {return sts.read<reg::peak_address>();}
+    uint32_t get_peak_maximum() {return sts.read<reg::peak_maximum>();}
 
     uint64_t get_counter() {
-        uint64_t lsb = sts.read<COUNTER0_OFF>();
-        uint64_t msb = sts.read<COUNTER1_OFF>();
+        uint64_t lsb = sts.read<reg::counter0>();
+        uint64_t msb = sts.read<reg::counter1>();
         return lsb + (msb << 32);
     }
 
@@ -100,23 +100,23 @@ class Spectrum
     bool fifo_get_acquire_status()                   {return fifo.get_acquire_status();}
 
   private:
-    MemoryMap<CONFIG_MEM>& cfg;
-    MemoryMap<STATUS_MEM>& sts;
-    MemoryMap<SPECTRUM_MEM>& spectrum_map;
-    MemoryMap<DEMOD_MEM>& demod_map;
-    MemoryMap<NOISE_FLOOR_MEM>& noise_floor_map;
+    MemoryMap<mem::config>& cfg;
+    MemoryMap<mem::status>& sts;
+    MemoryMap<mem::spectrum>& spectrum_map;
+    MemoryMap<mem::demod>& demod_map;
+    MemoryMap<mem::noise_floor>& noise_floor_map;
 
     // Acquired data buffers
     float *raw_data;
     std::array<float, WFM_SIZE> spectrum_data;
     std::vector<float> spectrum_decim;
-    FIFOReader<PEAK_FIFO_MEM, FIFO_BUFF_SIZE> fifo;
-    DacRouter<N_DAC_PARAM, N_DAC_BRAM_PARAM> dac;
+    FIFOReader<mem::peak_fifo, FIFO_BUFF_SIZE> fifo;
+    DacRouter<prm::n_dac, prm::n_dac_bram> dac;
 
     // Internal functions
     void wait_for_acquisition() {
-       do {} while (sts.read<AVG_READY_OFF>() == 0);
+       do {} while (sts.read<reg::avg_ready>() == 0);
     }
 }; // class Spectrum
 
-#endif // __DRIVERS_CORE_SPECTRUM_HPP__
+#endif // __DRIVERS_SPECTRUM_HPP__
