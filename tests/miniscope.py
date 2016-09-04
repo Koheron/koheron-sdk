18810d481354dbce76d6430b02f4c64a3ac03e98
thomas <th.vanderbruggen@gmail.com>
2016-09-04 18:22:24 +0200

    Clean up

--- tests/miniscope.py
+++ tests/miniscope.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Function generator with live acquisition plot
+
 import context
 import os
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
@@ -15,39 +16,53 @@ host = os.getenv('HOST','192.168.1.100')
 client = load_instrument(host, instrument='oscillo')
 driver = Oscillo(client)
 
-n = driver.wfm_size
+n = driver.wfm_size # Number of samples
+fs = 125E6          # Sampling rate
+n_bits = 14
 
-freq = 1
+# -----------------------------------------------------------------------------------------------------
+# Waveform generation
+# -----------------------------------------------------------------------------------------------------
+
+freq = 10
 mod_amp = 0.2
+waveform = 'sine'
+dac_channel = 0
 
-# Modulate with a triangle waveform of period 8192 x 8 ns
-driver.dac[0,:] = mod_amp * signal.sawtooth(2 * np.pi * freq / n * np.arange(n), width=0.5)
-driver.dac[1,:] = mod_amp * signal.sawtooth(2 * np.pi * freq / n * np.arange(n), width=0.5)
+if waveform == 'triangle':
+    driver.dac[dac_channel,:] = mod_amp * signal.sawtooth(2 * np.pi * freq / n * np.arange(n), width=0.5)
+elif waveform == 'sine':
+    driver.dac[dac_channel,:] = mod_amp * np.sin(2 * np.pi * freq / n * np.arange(n))
+elif waveform == 'square':
+    driver.dac[dac_channel,:] = mod_amp * signal.square(2 * np.pi * freq / n * np.arange(n))
+
 driver.set_dac()
 
-driver.set_averaging(True)
-time.sleep(0.1)
+# -----------------------------------------------------------------------------------------------------
+# Data acquisition
+# -----------------------------------------------------------------------------------------------------
 
+adc_channel = 0
+
+driver.set_averaging(True)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-x = np.arange(n)
-y = np.ones(n)
-li, = ax.plot(x, y)
+t = 1E6 * np.arange(n) / fs # Time axis in us
+y = np.zeros(n)
+li, = ax.plot(t, y)
+ax.set_xlabel('Time (us)')
+ax.set_ylabel('Signal (V)')
 fig.canvas.draw()
 plt.show(block=False)
 
 while True:
     try:
         driver.get_adc()
-        y = driver.adc[0,:]
-
-        li.set_ydata(y)
+        li.set_ydata(driver.adc[adc_channel,:] / 2**n_bits)
         ax.relim()
-        ax.autoscale_view(True,True,True)
+        ax.autoscale_view(True, True, True)
         fig.canvas.draw()
-        # plt.plot(x, y)
-        # plt.draw()
 
     except KeyboardInterrupt:
         break
