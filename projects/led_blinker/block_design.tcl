bcd4fde3dd85df6edd013d27de43e09f7c5bbdd0
jeanminet <jean.minet@gmail.com>
2016-09-10 17:02:40 +0200

    add led_blinker project

---
+++ projects/led_blinker/block_design.tcl
@@ -0,0 +1,14 @@
+# Add PS and AXI Interconnect
+set board_preset boards/$board_name/config/board_preset.tcl
+source $lib/starting_point.tcl
+
+# Add config and status registers
+source $lib/cfg_sts.tcl
+add_cfg_sts
+
+# Connect LEDs to config register
+create_bd_port -dir O -from 7 -to 0 led_o
+connect_bd_net [get_bd_ports led_o] [get_bd_pins [get_slice_pin [cfg_pin led] 7 0]]
+
+# Connect 42 to status register
+connect_pins [get_constant_pin 42 32] [sts_pin forty_two]
