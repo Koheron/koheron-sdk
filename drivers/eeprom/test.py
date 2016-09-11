0aca1e771f014b43dce1a41f85da95144a3917d6
MinJung <minjung.kim@koheron.com>
2016-09-11 18:02:25 +0200

    Update drivers

--- drivers/eeprom/test.py
+++ drivers/eeprom/test.py
@@ -5,8 +5,8 @@ from koheron import load_instrument
 from eeprom import Eeprom
 
 host = os.getenv('HOST','192.168.1.100')
-project = os.getenv('NAME','')
-client = load_instrument(host, project)
+instrument = os.getenv('NAME','')
+client = load_instrument(host, instrument)
 
 eeprom = Eeprom(client)
 
