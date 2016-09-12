4b307e95a4e50d0bd7681732b2b606d98b8bad54
thomas <th.vanderbruggen@gmail.com>
2016-09-12 12:46:33 +0200

    Fix tcl scripts

--- fpga/scripts/hwdef.tcl
+++ fpga/scripts/hwdef.tcl
@@ -1,7 +1,7 @@
 
 set instrument_name [lindex $argv 0]
 
-open_instrument tmp/$instrument_name.xpr
+open_project tmp/$instrument_name.xpr
 
 if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
   launch_runs synth_1
