25593f17a990c7b1cbb12a5081e923463418617a
thomas <th.vanderbruggen@gmail.com>
2016-09-14 12:08:25 +0200

    Clean up

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM jeanminet/tcp-server
-
-ENV work_dir /__code
-
-WORKDIR $work_dir/
-COPY . $work_dir/
--- scripts/tests/ci_tests.sh
+++ scripts/tests/ci_tests.sh
@@ -1,7 +1,5 @@
 set -e
 
-pip install -r requirements.txt
-
 python scripts/make.py --test
 
 for instrument in led_blinker adc_dac pulse_generator oscillo spectrum
