df238ee7ca64b7089ca2e77397a1ec8af2c6b9d9
jeanminet <jean.minet@gmail.com>
2016-09-16 21:48:04 +0200

    add dac_router module and remove base project

--- instruments/spectrum/block_design.tcl
+++ instruments/spectrum/block_design.tcl
@@ -1,4 +1,4 @@
-source instruments/base/block_design.tcl
+source instruments/oscillo/init.tcl
 source instruments/spectrum/spectrum.tcl
-source instruments/base/timer.tcl
+source instruments/oscillo/timer.tcl
 source fpga/lib/at93c46d.tcl
