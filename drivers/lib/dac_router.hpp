eac9aad3a1b68399678a7c37581577fc26c921b9
Jean Minet <jean.minet@gmail.com>
2016-09-21 11:15:02 +0200

    clean dac_router (#323)

--- drivers/lib/dac_router.hpp
+++ drivers/lib/dac_router.hpp
@@ -36,10 +36,10 @@ class DacRouter
     /// /!\ Array is of size 1/2 of the number of samples in the waveform
     template<size_t N>
     void set_data(uint32_t channel, const std::array<uint32_t, N> arr) {
-        uint32_t old_idx = bram_index[channel];
-        uint32_t new_idx = get_first_empty_bram_index();
+        uint32_t new_idx = get_first_empty_bram();
         dac_map.set_ptr(arr.data(), N, new_idx);
-        switch_interconnect(channel, old_idx, new_idx);
+        bram_index[channel] = new_idx;
+        update_dac_routing();
     }
 
   private:
@@ -48,16 +48,11 @@ class DacRouter
     uint32_t addr_select_off;
     Memory<mem::dac>& dac_map;
     std::array<uint32_t, n_dac> bram_index;
-    std::array<bool, n_dac_bram> connected_bram;
 
     void init_dac_brams() {
         // Use BRAM0 for DAC0, BRAM1 for DAC1 ...
         for (uint32_t i=0; i < n_dac; i++) {
             bram_index[i] = i;
-            connected_bram[i] = true;
-        }
-        for (uint32_t i=n_dac; i < n_dac_bram; i++) {
-            connected_bram[i] = false;
         }
         update_dac_routing();
     }
@@ -65,7 +60,7 @@ class DacRouter
     void update_dac_routing() {
         // dac_select defines the connection between BRAMs and DACs
         uint32_t dac_select = 0;
-        for (uint32_t i=0; i < n_dac_bram; i++) {
+        for (uint32_t i=0; i < n_dac; i++) {
             dac_select += bram_index[i] << (get_width(n_dac_bram) * i);
         }
         cfg.write_reg(dac_select_off, dac_select);
@@ -73,12 +68,21 @@ class DacRouter
         // addr_select defines the connection between address generators and BRAMs
         uint32_t addr_select = 0;
         for (uint32_t j=0; j < n_dac_bram; j++) {
-            addr_select += j << (get_width(n_dac) * bram_index[j]);
+            addr_select += get_addr_index(j) << (get_width(n_dac) * j);
         }
         cfg.write_reg(addr_select_off, addr_select);
     }
 
-    uint32_t get_first_empty_bram_index() {
+    uint32_t get_addr_index(uint32_t i) {
+        for (uint32_t j=0; j < n_dac; j++) {
+            if (bram_index[j] == i) {
+                return j;
+            }
+        }
+        return 0;
+    }
+
+    uint32_t get_first_empty_bram() {
         for (uint32_t i=0; i < n_dac_bram; i++) {
             // Check if the BRAM is connected to no
             bool empty = true;
@@ -93,13 +97,6 @@ class DacRouter
             }
         }
         return -1;
-    }
-
-    void switch_interconnect(uint32_t channel, uint32_t old_idx, uint32_t new_idx) {
-        bram_index[channel] = new_idx;
-        connected_bram[new_idx] = true;
-        update_dac_routing();
-        connected_bram[old_idx] = false;
     }
 
 };
