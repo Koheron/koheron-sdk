e1497c09f3ba140cc8d77c89efee349b6965abf1
jeanminet <jean.minet@gmail.com>
2016-09-23 15:24:03 +0200

    pdm timing ok @250MHz

--- fpga/cores/pdm_v1_0/pdm.v
+++ fpga/cores/pdm_v1_0/pdm.v
@@ -16,18 +16,27 @@ module pdm #
 );
  
   localparam integer MAX = 2**NBITS - 1; 
+  reg [NBITS-1:0] data_in_reg;
+  reg [NBITS-1:0] pdm_error_0;
+  reg [NBITS-1:0] pdm_error_1;
+  
+  always @(posedge clk) begin
+    data_in_reg <= data_in;
+    pdm_error_1 <= pdm_error + MAX - data_in_reg;
+    pdm_error_0 <= pdm_error - data_in_reg;
+  end
   
   always @(posedge clk) begin
     if (rst == 1'b1) begin
       pdm_out <= 0;
       pdm_error <= 0;
     end  
-    else if (data_in >= pdm_error) begin
+    else if (data_in_reg >= pdm_error) begin
       pdm_out <= 1;
-      pdm_error <= pdm_error + MAX - data_in;
+      pdm_error <= pdm_error_1;
     end else begin
       pdm_out <= 0;
-      pdm_error <= pdm_error - data_in;
+      pdm_error <= pdm_error_0;
     end
   end
 
