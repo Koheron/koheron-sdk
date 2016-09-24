aadcf0d702db5ace43840d6fc0b1cd768959d923
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2016-09-24 15:33:28 +0200

    Add modules in ci (#326)
    * Add modules in ci
    
    * Typo

--- scripts/tests/ci_tests.sh
+++ scripts/tests/ci_tests.sh
@@ -1,6 +1,17 @@
 set -e
 
+echo 'Test config.yml'
 python scripts/make.py --test
+
+# Test modules
+
+for module in address averager peak_detector spectrum
+do
+	echo 'Test config.yml for module' $module
+	python scripts/make.py --split_config_yml $module fpga/modules/
+done
+
+# Test instrument drivers
 
 for instrument in led_blinker adc_dac laser_controller pulse_generator oscillo spectrum
 do
