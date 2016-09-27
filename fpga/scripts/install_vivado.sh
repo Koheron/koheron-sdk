9b12d5f1d921e9c4cc206988b89a32a9db6f7857
Jean Minet <jean.minet@gmail.com>
2016-09-27 18:20:44 +0200

    fix Vivado glibc problem

--- fpga/scripts/install_vivado.sh
+++ fpga/scripts/install_vivado.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-vivado_version=2016.2_0605_1
+vivado_release=2016.2
+vivado_version=${vivado_release}_0605_1
 
 tar -xvzf Xilinx_Vivado_SDK_${vivado_version}.tar.gz
 
@@ -37,4 +38,11 @@ EOF_CAT
 bash Xilinx_Vivado_SDK_${vivado_version}/xsetup --agree 3rdPartyEULA,WebTalkTerms,XilinxEULA --batch Install --config install_config.txt
 rm install_config.txt
 rm -r Xilinx_Vivado_SDK_${vivado_version}
-# sudo cp Xilinx.lic /opt/Xilinx
+
+# Use system version of glibc
+for folder in Vivado SDK
+do
+  path=/opt/Xilinx/${folder}/${vivado_release}/lib/lnx64.o
+  mv $path/libstdc++.so.6 $path/libstdc++.so.6.orig
+  ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.21 $path/libstdc++.so.6
+done
