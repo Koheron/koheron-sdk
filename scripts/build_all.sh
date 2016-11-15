7de706757fd70c9d0b24e0bb788c61d2b6f5d70d
jeanminet <jean.minet@gmail.com>
2016-11-15 14:42:25 +0100

    specify version of koheron-server and koheron-python

---
+++ scripts/build_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+. settings.sh
+
+make NAME=led_blinker linux
+make NAME=oscillo
+make NAME=spectrum
+make NAME=decimator
+make NAME=laser_controller
+make NAME=pulse_generator
+make NAME=adc_dac
+
+/bin/bash os/scripts/image.sh led_blinker
