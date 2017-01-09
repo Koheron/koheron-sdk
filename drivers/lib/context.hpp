5a5d3701903f0c5a919675c0cb0cb17539ff4ee1
thomas <th.vanderbruggen@gmail.com>
2017-01-09 14:51:35 +0100

    Add SPI to Context

--- drivers/lib/context.hpp
+++ drivers/lib/context.hpp
@@ -6,6 +6,8 @@
 #include <core/context_base.hpp>
 
 #include <drivers/lib/memory_manager.hpp>
+#include <drivers/lib/spi_dev.hpp>
+
 #include "memory.hpp"
 
 class Context : public ContextBase
@@ -13,13 +15,18 @@ class Context : public ContextBase
   public:
     Context()
     : mm()
+    , spi(*this)
     {}
 
     int init() {
-        return mm.open();
+        if (mm.open() < 0 || spi.init() < 0)
+            return -1;
+
+        return 0;
     }
 
     MemoryManager mm;
+    SpiDev spi;
 };
 
 #endif // __CONTEXT_HPP__
--- drivers/lib/spi_dev.cpp
+++ drivers/lib/spi_dev.cpp
@@ -1,25 +1,47 @@
 
-// From http://redpitaya.com/examples-new/spi/
-// See also https://www.kernel.org/doc/Documentation/spi/spidev
-
 #include "spi_dev.hpp"
 
-int SpiDev::init()
-{
-    fd = open("/dev/spidev2.0", O_RDWR | O_NOCTTY);
+#include <context.hpp>
 
+SpiDev::SpiDev(Context& ctx_, uint32_t mode_, uint32_t speed_)
+: ctx(ctx_)
+, mode(mode_)
+, speed(speed_)
+{}
+
+int SpiDev::init() {
     if (fd < 0) {
-        fprintf(stderr, "Cannot open /dev/spidev2.0\n");
-        return -1;
+        fd = open("/dev/spidev2.0", O_RDWR | O_NOCTTY);
+
+        if (fd < 0) {
+            ctx.log<ERROR>("Cannot open /dev/spidev2.0\n");
+            return -1;
+        }
     }
+
+    if (set_mode(mode) < 0 || set_speed(speed) < 0)
+        return -1;
+
+    ctx.log<INFO>("/dev/spidev2.0 opened\n");
+    return 0;
+}
+
+int SpiDev::set_mode(uint32_t mode_) {
+    mode = mode_;
 
     if (ioctl(fd, SPI_IOC_WR_MODE, &mode) < 0) {
-        fprintf(stderr, "Cannot set ioctl SPI_IOC_WR_MODE\n");
+        ctx.log<ERROR>("Cannot set ioctl SPI_IOC_WR_MODE\n");
         return -1;
     }
 
+    return 0;
+}
+
+int SpiDev::set_speed(uint32_t speed_) {
+    speed = speed_;
+
     if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0) {
-        fprintf(stderr, "Cannot set ioctl SPI_IOC_WR_MAX_SPEED_HZ\n");
+        ctx.log<ERROR>("Cannot set ioctl SPI_IOC_WR_MAX_SPEED_HZ\n");
         return -1;
     }
 
