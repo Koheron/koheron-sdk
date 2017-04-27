da2cfab531af1461a6501777f1bc9535c7cba6e5
minjung <minjung.kim@koheron.com>
2017-04-27 18:27:16 +0200

    Update spectrum python test

--- examples/spectrum/python/test.py
+++ examples/spectrum/python/test.py
@@ -25,27 +25,15 @@ sampling_rate = 125e6
 spectrum.reset_acquisition()
 
 decimation_factor = 1
-index_low = 1
+index_low = 0
 index_high = wfm_size / 2
 
 decimated_data = spectrum.get_decimated_data(decimation_factor, index_low, index_high)
 
-print(decimated_data)
-
 freq_min = 0
 freq_max = sampling_rate / mhz / 2
 
-freq_range = np.linspace(freq_min, freq_max, wfm_size)
+freq_range = np.linspace(freq_min, freq_max, (wfm_size / 2))
 
-plot_data = np.zeros((wfm_size, 2))
-
-for i in range(0, len(decimated_data) - 1):
-    freq = (index_low + i) * sampling_rate / wfm_size / mhz
-    psd = 10 * math.log10(decimated_data[i])
-    plot_data[i] = [freq, psd]
-
-print(plot_data)
-
-plt.plot(freq_range, plot_data, 'b')
-plt.axis([0, 62.5, 125, 175])
+plt.plot(freq_range, 10 * np.log10(decimated_data), 'b')
 plt.show()
