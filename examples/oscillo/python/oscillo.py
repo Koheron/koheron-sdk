f15fdb8311e305c4379c0b02dcd5a15d9451a396
minjung <minjung.kim@koheron.com>
2017-04-27 17:07:40 +0200

    Clean up oscillo python

--- examples/oscillo/python/oscillo.py
+++ examples/oscillo/python/oscillo.py
@@ -10,14 +10,6 @@ from koheron import command
 class Oscillo(object):
     def __init__(self, client, verbose=False):
         self.client = client
-        self.wfm_size = 8192
-        self.avg_on = False
-
-        self.period = self.wfm_size
-        self.adc = np.zeros((2, self.wfm_size))
-        self.spectrum = np.zeros((2, self.wfm_size / 2))
-        self.avg_spectrum = np.zeros((2, self.wfm_size / 2))
-        self.dac = np.zeros((2, self.wfm_size))
 
     @command()
     def set_dac_periods(self, period0, period1):
@@ -34,30 +26,10 @@ class Oscillo(object):
         pass
 
     @command()
-    def set_avg_period(self, avg_period):
+    def set_average_period(self, average_period):
         ''' Set the period of the averaging module and reset the module.
         '''
-        self.period = avg_period
-
-    def set_dac(self, channels=[0,1]):
-        ''' Write the BRAM corresponding on the selected channels
-        (dac0 or dac1) with the array stored in self.dac[channel,:].
-        ex: self.set_dac(channel=[0])
-        '''
-        @command()
-        def set_dac_buffer(self, channel, arr):
-            pass
-        for channel in channels:
-            data = np.int16(16384 * (self.dac[channel,:]))
-            set_dac_buffer(self, channel, np.uint32(data[1::2] + data[::2] * 65536))
-
-    @command()
-    def set_dac_float(self, channel, arr):
-        pass
-
-    @command()
-    def get_dac_buffer(self, channel):
-        return self.client.recv_array(self.wfm_size / 2, dtype='uint32')
+        self.period = average_period
 
     @command()
     def set_average(self, is_average):
@@ -67,34 +39,16 @@ class Oscillo(object):
     @command()
     def get_num_average(self, channel):
         ''' Get the number of averages corresponding to the last acquisition. '''
-        n_avg = self.client.recv_uint32()
-        return n_avg
+        num_average = self.client.recv_uint32()
+        return num_average
 
     @command()
-    def read_all_channels(self):
-        return self.client.recv_array(2 * self.wfm_size, dtype='float32')
-
-    def get_adc(self):
-        ''' Read adc data and store it in self.adc. '''
-        data = self.read_all_channels()
-        self.adc = np.reshape(data, (2, self.wfm_size))
-
-    # -------------------------------
-    # Trigger related functions
-
-    @command()
-    def get_counter(self):
-        ''' Return a 64 bits integer that counts the number of clock
-        cycles (8 ns) between the startup of the FPGA and the last trigger
-        (beginning of the last acquisition).
-        '''
-        return self.client.recv_int(8, fmt='Q')
+    def get_decimated_data(self, decim_factor, index_low, index_high):
+        decimated_data = self.client.recv_vector(dtype='float32')
+        return decimated_data
 
     @command()
     def reset_acquisition(self):
-        ''' This function has the same effect as get_adc()
-        except it does not return any data
-        '''
         pass
 
     @command(funcname='reset')
@@ -103,8 +57,3 @@ class Oscillo(object):
 
     def reset(self):
         self.reset_dac()
-
-    @command()
-    def get_first_empty_bram_index(self):
-        return self.client.recv_uint32()
-
--- examples/oscillo/python/test.py
+++ examples/oscillo/python/test.py
@@ -1,8 +1,16 @@
-import os
-import numpy as np
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from koheron import connect
 from oscillo import Oscillo
+
+import os
+import numpy as np
+import math
+
+import matplotlib
+matplotlib.use('TKAgg')
+from matplotlib import pyplot as plt
 
 host = os.getenv('HOST','192.168.1.100')
 client = connect(host, name='oscillo')
@@ -10,40 +18,35 @@ client = connect(host, name='oscillo')
 oscillo = Oscillo(client)
 oscillo.reset()
 
-class TestsOscillo:
-    def test_get_adc(self):
-        oscillo.get_adc()
-        assert np.shape(oscillo.adc) == (2, oscillo.wfm_size)
-        assert np.amax(oscillo.adc[0,:]) <= 8191
-        assert np.amin(oscillo.adc[0,:]) >= -8192
-        assert np.amax(oscillo.adc[1,:]) <= 8191
-        assert np.amin(oscillo.adc[1,:]) >= -8192
+wfm_size = 8192
+mhz = 1e6
+sampling_rate = 125e6
 
-    def test_averaging(self):
-        oscillo.set_average(False)
-        assert oscillo.get_num_average(0) == oscillo.get_num_average(1)
-        oscillo.set_average(True)
-        oscillo.set_num_average_min(1000)
-        oscillo.get_adc()
-        num_avg_0 = oscillo.get_num_average(0)
-        assert num_avg_0 == oscillo.get_num_average(1)
-        assert num_avg_0 >= 1000
+decimation_factor = 1
+index_low = 0
+index_high = 8191
 
-    def test_set_dac(self):
-        data_send = np.arange(oscillo.wfm_size, dtype='uint32')
-        oscillo.dac[0, :] = data_send
-        oscillo.dac[1, :] = data_send
-        oscillo.set_dac([0,1])
-        data_read = oscillo.get_dac_buffer(1)
-        data_tmp = np.uint32(np.mod(np.floor(8192 * data_send) + 8192, 16384) + 8192)
-        data_read_expect = data_tmp[::2] + (data_tmp[1::2] << 16)
-        assert np.array_equal(data_read, data_read_expect)
-        assert np.array_equal(oscillo.get_dac_buffer(0), oscillo.get_dac_buffer(1))
+decimated_data = oscillo.get_decimated_data(decimation_factor, index_low, index_high)
 
-    def test_set_dac_float(self):
-        data_send = np.float32(np.sin(np.arange(oscillo.wfm_size)))
-        oscillo.set_dac_float(1, data_send)
-        data_read = oscillo.get_dac_buffer(1)
-        data_tmp = np.uint32(np.mod(np.floor(8192 * data_send) + 8192, 16384) + 8192)
-        data_read_expect = data_tmp[::2] + (data_tmp[1::2] << 16)
-        assert np.array_equal(data_read, data_read_expect)
+t_min = index_low * mhz / sampling_rate
+t_max = index_high * mhz / sampling_rate
+
+t_range = np.linspace(t_min, t_max, wfm_size)
+
+channel0 = np.zeros((wfm_size, 2))
+channel1 = np.zeros((wfm_size, 2))
+
+coeff = mhz * decimation_factor / sampling_rate
+size = int(math.floor((index_high - index_low) / decimation_factor))
+
+for i in range(0, wfm_size - 1):
+    channel0[i] = [t_min + coeff * i, decimated_data[i]]
+    channel1[i] = [t_min + coeff * i, decimated_data[i + size]]
+
+oscillo.set_num_average_min(2000)
+oscillo.set_average(True)
+
+plt.plot(t_range, channel0, 'b')
+plt.plot(t_range, channel1, 'g')
+plt.axis([50, 50.30, -1500, 1500])
+plt.show()
