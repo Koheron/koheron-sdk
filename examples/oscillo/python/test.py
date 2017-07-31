45992b59909ae9f87ec953b100ab1a9a12b4a536
Jean Minet <jean.minet@gmail.com>
2017-07-31 17:17:40 +0200

    fix oscillo python api

--- examples/oscillo/python/test.py
+++ examples/oscillo/python/test.py
@@ -4,6 +4,8 @@
 from koheron import connect
 from oscillo import Oscillo
 
+import time
+
 import os
 import numpy as np
 import math
@@ -15,26 +17,33 @@ from matplotlib import pyplot as plt
 host = os.getenv('HOST','192.168.1.100')
 client = connect(host, name='oscillo')
 
-oscillo = Oscillo(client)
-oscillo.reset()
-
-wfm_size = 8192
-mhz = 1e6
-sampling_rate = 125e6
+driver = Oscillo(client)
+driver.reset()
 
 decimation_factor = 1
 index_low = 0
-index_high = 8191
+index_high = 8192
 
 size = int(math.floor((index_high - index_low) / decimation_factor))
-t_min = index_low * mhz / sampling_rate
-t_max = index_high * mhz / sampling_rate
+t_min = 1e6*index_low / driver.sampling_rate # µs
+t_max = 1e6*index_high / driver.sampling_rate # µs
 t = np.linspace(t_min, t_max, size)
 
-oscillo.set_num_average_min(2000)
-oscillo.set_average(True)
+# Modulation on DAC
+amp_mod = 0.2
+freq_mod = driver.sampling_rate / driver.wfm_size * 10
+driver.dac[0, :] = amp_mod*np.cos(2 * np.pi * freq_mod * driver.t)
+driver.dac[1, :] = amp_mod*np.sin(2 * np.pi * freq_mod * driver.t)
+driver.set_dac()
 
-data = oscillo.get_decimated_data(decimation_factor, index_low, index_high)
+time.sleep(0.001)
+
+driver.set_num_average_min(2000)
+driver.set_average(True)
+
+data = driver.get_decimated_data(decimation_factor, index_low, index_high)
+
+print size
 
 data = np.reshape(data, (2, size))
 
