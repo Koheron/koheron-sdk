9962769b2d030674d2362a73169dddf1a703b12f
Jean Minet <jean.minet@gmail.com>
2017-07-31 17:20:40 +0200

    clean oscillo/python/test.py

--- examples/oscillo/python/test.py
+++ examples/oscillo/python/test.py
@@ -20,15 +20,6 @@ client = connect(host, name='oscillo')
 driver = Oscillo(client)
 driver.reset()
 
-decimation_factor = 1
-index_low = 0
-index_high = 8192
-
-size = int(math.floor((index_high - index_low) / decimation_factor))
-t_min = 1e6*index_low / driver.sampling_rate # µs
-t_max = 1e6*index_high / driver.sampling_rate # µs
-t = np.linspace(t_min, t_max, size)
-
 # Modulation on DAC
 amp_mod = 0.2
 freq_mod = driver.sampling_rate / driver.wfm_size * 10
@@ -41,14 +32,10 @@ time.sleep(0.001)
 driver.set_num_average_min(2000)
 driver.set_average(True)
 
-data = driver.get_decimated_data(decimation_factor, index_low, index_high)
+driver.get_adc()
 
-print size
-
-data = np.reshape(data, (2, size))
-
-plt.plot(t, data[0,:], 'b')
-plt.plot(t, data[1,:], 'g')
-plt.axis([t_min, t_max, -8192, 8192])
+plt.plot(1e6*driver.t, driver.adc[0,:], 'b')
+plt.plot(1e6*driver.t, driver.adc[1,:], 'g')
+plt.axis([0, 1e6*np.max(driver.t), -8192, 8192])
 plt.xlabel('Time (us)')
 plt.show()
