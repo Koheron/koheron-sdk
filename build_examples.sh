cb3efd3c1670433d43846bfae41b5a7b06bbab72
minjung <minjung.kim@koheron.com>
2017-11-19 16:31:49 +0100

    move redpitaya examples in directory

--- build_examples.sh
+++ build_examples.sh
@@ -4,11 +4,12 @@ set -e
 target=$1
 mode=development
 
-make CONFIG=examples/led-blinker/config.yml MODE=$mode $target
-make CONFIG=examples/oscillo/config.yml MODE=$mode $target
-make CONFIG=examples/spectrum/config.yml MODE=$mode $target
-make CONFIG=examples/decimator/config.yml MODE=$mode $target
-make CONFIG=examples/laser-controller/config.yml MODE=$mode $target
-make CONFIG=examples/pulse-generator/config.yml MODE=$mode $target
-make CONFIG=examples/adc-dac/config.yml MODE=$mode $target
-make CONFIG=examples/cluster/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/led-blinker/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/oscillo/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/spectrum/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/decimator/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/laser-controller/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/pulse-generator/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/adc-dac/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/cluster/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/dual-dds/config.yml MODE=$mode $target
