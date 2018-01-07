198eae5baeeffb33d82377a542bf78f9a75b7fc5
minjung <minjung.kim@koheron.com>
2018-01-07 17:22:48 +0100

    dual dds

--- examples/red-pitaya/dual-dds/web/control.ts
+++ examples/red-pitaya/dual-dds/web/control.ts
@@ -2,21 +2,15 @@
 // (c) Koheron
 
 class Control {
-    private frequency_value: HTMLLinkElement[];
-    private frequency_input: HTMLInputElement[];
-    private frequency_save: HTMLLinkElement[];
-    private frequency_slider: HTMLInputElement[];
+    private frequencyInputs: HTMLInputElement[];
+    private frequencySliders: HTMLInputElement[];
 
     constructor(document: Document, private driver: DualDDS) {
-        this.frequency_value = [];
-        this.frequency_input = [];
-        this.frequency_save = [];
-        this.frequency_slider = [];
+        this.frequencyInputs = [];
+        this.frequencySliders = [];
         for (let channel: number = 0; channel < 2; channel++) {
-            this.frequency_value[channel] = <HTMLLinkElement>document.getElementById('frequency-value-' + channel.toString());
-            this.frequency_input[channel] = <HTMLInputElement>document.getElementById('frequency-input-' + channel.toString());
-            this.frequency_save[channel] = <HTMLLinkElement>document.getElementById('frequency-save-' + channel.toString());
-            this.frequency_slider[channel] = <HTMLInputElement>document.getElementById('frequency-slider-'+ channel.toString());
+            this.frequencyInputs[channel] = <HTMLInputElement>document.getElementById('frequency-input-' + channel.toString());
+            this.frequencySliders[channel] = <HTMLInputElement>document.getElementById('frequency-slider-'+ channel.toString());
         }
         this.update();
     }
@@ -24,42 +18,29 @@ class Control {
     update() {
         this.driver.getControlParameters( (sts: DualDDSStatus) => {
             for (let channel: number = 0; channel < 2; channel++) {
-                this.frequency_value[channel].innerHTML = (sts.dds_freq[channel]/1e6).toFixed(8);
-                this.frequency_slider[channel].value = (sts.dds_freq[channel]/1e6).toFixed(8);
+                if (document.activeElement !== this.frequencyInputs[channel]) {
+                    this.frequencyInputs[channel].value = (sts.dds_freq[channel] / 1e6).toFixed(6);
+                }
+                if (document.activeElement !== this.frequencySliders[channel]) {
+                    this.frequencySliders[channel].value = (sts.dds_freq[channel]/1e6).toFixed(8);
+                }
             }
             requestAnimationFrame( () => { this.update(); } )
         });
     }
 
-    editFrequency(channel: number) {
-        this.frequency_value[channel].style.display = 'none';
-        this.frequency_input[channel].style.display = 'inline';
-        this.frequency_save[channel].style.display = 'inline';
-        this.frequency_input[channel].value = this.frequency_value[channel].innerHTML;
-    }
 
-    saveFrequency(channel: number) {
-        this.frequency_value[channel].style.display = 'inline';
-        this.frequency_input[channel].style.display = 'none';
-        this.frequency_save[channel].style.display = 'none';
-        let frequency = Math.min(parseFloat(this.frequency_input[channel].value), 62.5);
-        this.driver.setDDSFreq(channel, 1e6 * frequency);
-    }
+    setFrequency(channel: number, event) {
 
-    saveFrequencyKey(event: KeyboardEvent, channel: number) {
-        if (event.keyCode == 13) {
-            this.saveFrequency(channel);
+        let frequencyValue = event.value;
+
+        if (event.type === 'number') {
+            this.frequencySliders[channel].value = frequencyValue;
+        } else if (event.type === 'range') {
+            this.frequencyInputs[channel].value = frequencyValue;
         }
-    }
 
-    slideFrequency(channel: number) {
-        if (this.frequency_input[channel].style.display == 'inline') {
-            this.frequency_value[channel].style.display = 'inline';
-            this.frequency_input[channel].style.display = 'none';
-            this.frequency_save[channel].style.display = 'none';
-        }
-        let frequency = parseFloat(this.frequency_slider[channel].value);
-        this.driver.setDDSFreq(channel, 1e6 * frequency);
+        this.driver.setDDSFreq(channel, 1e6 * Math.min(parseFloat(frequencyValue), 62.5));
     }
 
 }
