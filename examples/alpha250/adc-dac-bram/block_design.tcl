549f5f3e6fabc61de41f260fc46a6be972ebc42b
Jean Minet <jean.minet@gmail.com>
2018-01-10 11:52:30 +0100

    add shift registers to fix timing in adc-dac-bram

--- examples/alpha250/adc-dac-bram/block_design.tcl
+++ examples/alpha250/adc-dac-bram/block_design.tcl
@@ -60,7 +60,7 @@ cell koheron:user:address_counter:1.0 address_counter {
 connect_cell blk_mem_gen_adc {
   addrb address_counter/address
   clkb adc_dac/adc_clk
-  dinb [get_concat_pin [list adc_dac/adc0 adc_dac/adc1]]
+  dinb [get_Q_pin [get_concat_pin [list adc_dac/adc0 adc_dac/adc1]] 1 noce adc_dac/adc_clk]
   enb [get_constant_pin 1 1]
   rstb [get_constant_pin 0 1]
   web address_counter/wen
@@ -74,5 +74,5 @@ connect_cell blk_mem_gen_dac {
   web [get_constant_pin 0 4]
 }
 
-connect_pins adc_dac/dac0 [get_slice_pin blk_mem_gen_dac/doutb 15 0]
-connect_pins adc_dac/dac1 [get_slice_pin blk_mem_gen_dac/doutb 31 16]
+connect_pins adc_dac/dac0 [get_Q_pin [get_slice_pin blk_mem_gen_dac/doutb 15 0] 1 noce adc_dac/adc_clk]
+connect_pins adc_dac/dac1 [get_Q_pin [get_slice_pin blk_mem_gen_dac/doutb 31 16] 1 noce adc_dac/adc_clk]
