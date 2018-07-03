7a43375444855d7b5901ffc7895bc3b1acf2b71a
Jean Minet <jean.minet@gmail.com>
2018-07-03 15:18:24 +0200

    Enable loadable module support

---
+++ boards/alpha250/board.mk
@@ -0,0 +1,8 @@
+# Linux and U-boot
+UBOOT_TAG := koheron-v$(VIVADO_VERSION)
+LINUX_TAG := koheron-v$(VIVADO_VERSION)-kernel_module
+DTREE_TAG := xilinx-v$(VIVADO_VERSION)
+
+UBOOT_URL := https://github.com/Koheron/u-boot-xlnx/archive/$(UBOOT_TAG).tar.gz
+LINUX_URL := https://github.com/Koheron/linux-xlnx/archive/$(LINUX_TAG).tar.gz
+DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
