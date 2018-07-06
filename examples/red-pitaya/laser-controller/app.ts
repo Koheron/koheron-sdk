f5078096f7e5bcd761bc991fef9a0efe2dea6f68
minjung <minjung.kim@koheron.com>
2018-07-06 20:41:26 +0200

    laser controller

--- examples/red-pitaya/laser-controller/app.ts
+++ examples/red-pitaya/laser-controller/app.ts
@@ -3,12 +3,14 @@ class App {
     public laserControl: LaserControl;
     private laserDriver: LaserDriver;
     private navigation: Navigation;
+    private imports: Imports;
 
     constructor(window: Window, document: Document, ip: string) {
         let client = new Client(ip, 5);
 
         window.addEventListener('load', () => {
             client.init( () => {
+                this.imports = new Imports(document);
                 this.laserDriver = new LaserDriver(client);
                 this.laserControl = new LaserControl(document, this.laserDriver);
                 this.navigation = new Navigation(document);
