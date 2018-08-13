369a2c22530a16f9331cf5dc2cba935cb96080ce
minjung <minjung.kim@koheron.com>
2018-08-13 18:11:53 +0200

    use imports pulse-generator, led-blinker

--- examples/red-pitaya/led-blinker/web/app.ts
+++ examples/red-pitaya/led-blinker/web/app.ts
@@ -2,12 +2,14 @@ class App {
     private driver: LedBlinker;
     public control: Control;
     private navigation: Navigation;
+    private imports: Imports;
 
     constructor(window: Window, document: Document, ip: string) {
         let client = new Client(ip, 5);
 
-        window.addEventListener('load', () => {
+        window.addEventListener('HTMLImportsLoaded', () => {
             client.init( () => {
+                this.imports = new Imports(document);
                 this.driver = new LedBlinker(client);
                 this.control = new Control(document, this.driver);
                 this.navigation = new Navigation(document);
