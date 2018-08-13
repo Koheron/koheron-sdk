bdff4ebdd95f9ddcca7c8bb6cfdbc58dec947693
minjung <minjung.kim@koheron.com>
2018-08-13 19:51:23 +0200

    mv imports.ts in koheron.ts and download polyfill

--- web/koheron.ts
+++ web/koheron.ts
@@ -677,3 +677,17 @@ function __range__(left, right, inclusive) {
   }
   return range;
 }
+
+class Imports {
+    private importLinks: HTMLLinkElement[];
+
+    constructor (document: Document) {
+      this.importLinks = <HTMLLinkElement[]><any>document.querySelectorAll('link[rel="import"]');
+      for (let i = 0; i < this.importLinks.length ; i++) {
+        let template = <any>this.importLinks[i].import.querySelector('.template');
+        let clone = document.importNode(template.content, true);
+        let parentId = this.importLinks[i].dataset.parent;
+        document.getElementById(parentId).appendChild(clone);
+      }
+    }
+  }
