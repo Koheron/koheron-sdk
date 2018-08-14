13d2e323e6edfb592856fb1c5f22c21903948710
minjung <minjung.kim@koheron.com>
2018-08-14 12:49:35 +0200

    update SDK version to 0.17.1 and instruments version

--- build_examples.sh
+++ build_examples.sh
@@ -8,6 +8,7 @@ make CONFIG=examples/alpha250/fft/config.yml MODE=$mode $target
 make CONFIG=examples/alpha250/loopback/config.yml MODE=$mode $target
 make CONFIG=examples/alpha250/adc-dac-bram/config.yml MODE=$mode $target
 make CONFIG=examples/alpha250/adc-dac-dma/config.yml MODE=$mode $target
+make CONFIG=examples/alpha250/phase-noise-analyzer/config.yml MODE=$mode $target
 make CONFIG=examples/red-pitaya/led-blinker/config.yml MODE=$mode $target
 make CONFIG=examples/red-pitaya/oscillo/config.yml MODE=$mode $target
 make CONFIG=examples/red-pitaya/spectrum/config.yml MODE=$mode $target
@@ -18,5 +19,6 @@ make CONFIG=examples/red-pitaya/adc-dac/config.yml MODE=$mode $target
 make CONFIG=examples/red-pitaya/cluster/config.yml MODE=$mode $target
 make CONFIG=examples/red-pitaya/dual-dds/config.yml MODE=$mode $target
 make CONFIG=examples/red-pitaya/fft/config.yml MODE=$mode $target
+make CONFIG=examples/red-pitaya/phase-noise-analyzer/config.yml MODE=$mode $target
 make CONFIG=examples/zedboard/led-blinker/config.yml MODE=$mode $target
 make CONFIG=examples/zedboard/picoblaze/config.yml MODE=$mode $target
