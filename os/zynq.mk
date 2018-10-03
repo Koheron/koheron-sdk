a1c0e05f0987ea5fded8f7d9c8d803b3e67b4265
Jean Minet <jean.minet@gmail.com>
2018-10-03 19:39:03 +0200

    try to make build compatible with Zynq ultrascale

---
+++ os/zynq.mk
@@ -0,0 +1,5 @@
+PROC := ps7_cortexa9_0
+GCC_FLAGS := -march=armv7-a -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard
+GCC_ARCH := arm-linux-gnueabihf
+IMAGE_NAME := uImage
+ARCH := arm
---
+++ os/zynqmp.mk
@@ -0,0 +1,5 @@
+PROC := psu_cortexa53_0
+GCC_FLAGS := -march=armv8-a -mcpu=cortex-a53
+GCC_ARCH := aarch64-linux-gnu
+IMAGE_NAME := Image
+ARCH := arm64
