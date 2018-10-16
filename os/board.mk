37c454e6282656945a3c4557432933dd2222a6b1
Jean Minet <jean.minet@gmail.com>
2018-10-16 19:39:44 +0200

    fix linux url

--- os/board.mk
+++ os/board.mk
@@ -4,5 +4,5 @@ LINUX_TAG := xilinx-v$(VIVADO_VERSION)
 DTREE_TAG := xilinx-v$(VIVADO_VERSION)
 
 UBOOT_URL := https://github.com/Xilinx/u-boot-xlnx/archive/$(UBOOT_TAG).tar.gz
-LINUX_URL := https://github.com/Koheron/linux-xlnx/archive/$(LINUX_TAG).tar.gz
+LINUX_URL := https://github.com/Xilinx/linux-xlnx/archive/$(LINUX_TAG).tar.gz
 DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
