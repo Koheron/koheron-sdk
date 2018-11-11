533c9fcffee511a05f5615988d1505837ec9ed95
thomas <th.vanderbruggen@gmail.com>
2018-11-11 21:40:40 +0100

    Start ZynqFclk driver

--- server/context/context.hpp
+++ server/context/context.hpp
@@ -8,6 +8,7 @@
 #include <memory_manager.hpp>
 #include <spi_dev.hpp>
 #include <i2c_dev.hpp>
+#include <zynq_fclk.hpp>
 
 #include "memory.hpp"
 
@@ -18,7 +19,10 @@ class Context : public ContextBase
     : mm()
     , spi(*this)
     , i2c(*this)
-    {}
+    , fclk(*this)
+    {
+        fclk.set_fclk("fclk0", 200000000);
+    }
 
     int init() {
         if (mm.open() < 0  ||
@@ -32,6 +36,7 @@ class Context : public ContextBase
     MemoryManager mm;
     SpiManager spi;
     I2cManager i2c;
+    ZynqFclk fclk;
 };
 
 #endif // __CONTEXT_HPP__
