36ebed829211a6033e366c3f695ed089b2ce55dd
thomas <th.vanderbruggen@gmail.com>
2019-03-11 17:43:36 +0100

    Fix memory leak in SessionManager

--- server/core/session_abstract.hpp
+++ server/core/session_abstract.hpp
@@ -15,6 +15,8 @@ class SessionAbstract
     explicit SessionAbstract(int socket_type_)
     : type(socket_type_) {}
 
+    virtual ~SessionAbstract() {}
+
     template<typename... Tp> std::tuple<int, Tp...> deserialize(Command& cmd);
     template<typename Tp> int recv(Tp& container, Command& cmd);
     template<uint16_t class_id, uint16_t func_id, typename... Args> int send(Args&&... args);
