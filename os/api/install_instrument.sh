8c86fefd478a16bdb32b18bcc5f2ccee14412435
rsarwar87 <40424194+rsarwar87@users.noreply.github.com>
2020-02-04 00:03:29 +0000

    support for fpga manager with overlay support.

--- os/api/install_instrument.sh
+++ os/api/install_instrument.sh
@@ -8,5 +8,29 @@ LIVE_DIRNAME=$2
 
 /bin/systemctl stop koheron-server.service
 /usr/bin/unzip -o /usr/local/instruments/${NAME}.zip -d ${LIVE_DIRNAME}
+
+echo 'Load bitstream'
+XDEV=/dev/xdevcfg
+FMAN=/sys/class/fpga_manager/fpga0/firmware
+if [ -f "$XDEV" ]; then
+    echo "$XDEV exist"
+    /bin/cat ${LIVE_DIRNAME}/${NAME}.bit > /dev/xdevcfg
+    /bin/cat /sys/bus/platform/drivers/xdevcfg/f8007000.devcfg/prog_done
+elif [ -f "$XDEV" ]; then
+    echo "$FMAN exist"
+    mkdir -p /config
+    mkdir -p /lib/firmware
+    mount -t configfs configfs /configfs
+    mkdir -p /configfs/device-tree/overlays/full
+    echo 0 > /sys/class/fpga_manager/fpga0/flags
+    cp ${LIVE_DIRNAME}/overlay.dtb /lib/firmware/.
+    cp ${LIVE_DIRNAME}/*.bit.bin /lib/firmware/.
+    echo -n "overlay.dtb" > /configfs/device-tree/overlays/full/path
+else
+    echo "Could not determine fpga manager/xdevcfg"
+    exit -1
+fi
+
+echo 'Restart koheron-server'
 /bin/systemctl start koheron-server.service
 /bin/systemctl start koheron-server-init.service
