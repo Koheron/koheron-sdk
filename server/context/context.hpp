267160a648a226c7a900aca25b552e3e01ba2b02
jeanminet <jean.minet@gmail.com>
2020-06-30 20:16:10 +0200

    load bitstream from bash instead of cpp for now

--- server/context/context.hpp
+++ server/context/context.hpp
@@ -23,10 +23,10 @@ class Context : public ContextBase
     , fclk(*this)
     , fpga(*this)
     {
-        if (fpga.load_bitstream(instrument_name) < 0) {
-            log<PANIC>("Failed to load bitstream. Exiting server...\n");
-            exit(EXIT_FAILURE);
-        }
+        //if (fpga.load_bitstream(instrument_name) < 0) {
+        //    log<PANIC>("Failed to load bitstream. Exiting server...\n");
+        //    exit(EXIT_FAILURE);
+        //}
 
         // We set all the Zynq clocks before starting the drivers
         zynq_clocks::set_clocks(fclk);
