23d73b118915cc3a298e3096fb9b6ac200d0eee5
jean <jeanminet>
2020-06-30 19:05:28 +0200

    fix u-boot: ALPHA250 boot ok

--- boards/alpha250/patches/u-boot/include/configs/zynq_alpha250.h
+++ boards/alpha250/patches/u-boot/include/configs/zynq_alpha250.h
@@ -18,7 +18,6 @@
     "fdt_high=0x1E000000\0" \
     "preboot=env import -t 0xFFFFFC00\0" \
     "sdboot=echo Importing environment from SD... && mmcinfo && fatload mmc 0 0x2000000 uEnv.txt && env import -t 0x2000000 ${filesize} && boot"
-#define CONFIG_ZYNQ_SDHCI0
 /* Select Micrel PHY */
 #define CONFIG_PHY_MICREL
 #define PHY_ANEG_TIMEOUT  8000  /* PHY needs a longer aneg time */
