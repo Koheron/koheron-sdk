7bffc26aee676e0f02761b074b3cd065a7b09590
jeanminet <jean.minet@gmail.com>
2020-09-22 11:26:28 +0200

    add alpha250-1g to boards

---
+++ boards/alpha250-1g/board.mk
@@ -0,0 +1,10 @@
+# Linux and U-boot
+UBOOT_CONFIG = zynq_alpha250_1g_defconfig  
+ZYNQ_TYPE ?= zynq
+UBOOT_TAG := xilinx-uboot-v$(VIVADO_VERSION)
+LINUX_TAG := xilinx-linux-v$(VIVADO_VERSION)
+DTREE_TAG := xilinx-v$(VIVADO_VERSION)
+
+UBOOT_URL := https://github.com/Xilinx/u-boot-xlnx/archive/xilinx-v$(VIVADO_VERSION).tar.gz
+LINUX_URL := https://github.com/Xilinx/linux-xlnx/archive/xilinx-v$(VIVADO_VERSION).tar.gz
+DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
