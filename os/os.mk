19e1dc1d8e49d12914f255f77b9375aa3126c030
Rashed <r.sarwar87@gmail.com>
2022-10-10 14:51:49 +0100

    update

--- os/os.mk
+++ os/os.mk
@@ -181,7 +181,7 @@ $(TMP_OS_PATH)/overlay/pl.dtsi: $(TMP_FPGA_PATH)/$(NAME).xsa $(DTREE_PATH) $(PAT
 	mkdir -p $(@D)
 	$(HSI) $(FPGA_PATH)/hsi/devicetree.tcl $(NAME) $(PROC) $(DTREE_PATH) $(VIVADO_VER) $(TMP_OS_PATH)/hard $(TMP_OS_PATH)/overlay $(TMP_FPGA_PATH)/$(NAME).xsa $(BOOT_MEDIUM)
 	cp -R $(TMP_OS_PATH)/overlay $(TMP_OS_PATH)/overlay.orig
-	patch -d $(TMP_OS_PATH) -p -0 < $(PATCHES)/overlay.patch 
+	patch -d $(TMP_OS_PATH) -p -0 < $(PROJECT_PATH)/overlay.patch 
 	@echo [$@] OK
 
 $(TMP_OS_PATH)/devicetree/system-top.dts: $(TMP_FPGA_PATH)/$(NAME).xsa $(DTREE_PATH) $(PATCHES)/devicetree.patch
@@ -201,7 +201,7 @@ patch_devicetree:
 
 .PHONY: patch_overlay
 patch_overlay:
-	bash os/scripts/patch_overlay.sh $(TMP_OS_PATH) $(BOARD_PATH)
+	bash os/scripts/patch_overlay.sh $(TMP_OS_PATH) $(PROJECT_PATH)
 
 .PHONY: clean_overlay
 clean_overlay:
@@ -239,7 +239,7 @@ $(TMP_PROJECT_PATH)/pl.dtbo: $(TMP_OS_PATH)/pl.dtbo
 
 $(TMP_OS_PATH)/pl.dtbo: $(TMP_OS_PATH)/overlay/pl.dtsi
 	sed -i 's/".bin"/"$(NAME).bit.bin"/g' $(TMP_OS_PATH)/overlay/pl.dtsi
-	dtc -O dtb -o $@ \
+	$(LINUX_PATH)/scripts/dtc/dtc -O dtb -o $@ \
 	  -i $(TMP_OS_PATH)/overlay -b 0 -@ $(TMP_OS_PATH)/overlay/pl.dtsi
 	@echo [$@] OK
 
