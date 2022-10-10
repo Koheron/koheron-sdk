7438b844192d616c8f2cabb4e40f753eee844b6b
Rashed <r.sarwar87@gmail.com>
2022-10-10 14:47:24 +0100

    update compilation tags

--- os/zynq.mk
+++ os/zynq.mk
@@ -1,5 +1,5 @@
 PROC := ps7_cortexa9_0
-GCC_FLAGS := -march=armv7-a -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard
+GCC_FLAGS := -march=armv7-a -mfpu=neon -mfloat-abi=hard
 GCC_ARCH := arm-linux-gnueabihf
 LINUX_IMAGE := uImage
 ARCH := arm
