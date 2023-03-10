51f62f158113bdcd100fd6707f4ea66e1d0777ab
jeanminet <jean.minet@gmail.com>
2023-03-10 12:59:13 +0100

    use os/alpha/fsbl

--- boards/alpha15/board.mk
+++ boards/alpha15/board.mk
@@ -6,3 +6,5 @@ DTREE_TAG := xilinx-v$(VIVADO_VERSION)
 UBOOT_URL := https://github.com/Koheron/u-boot-xlnx/archive/$(UBOOT_TAG).tar.gz
 LINUX_URL := https://github.com/Koheron/linux-xlnx/archive/$(LINUX_TAG).tar.gz
 DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
+
+FSBL_PATH := $(OS_PATH)/alpha/fsbl
