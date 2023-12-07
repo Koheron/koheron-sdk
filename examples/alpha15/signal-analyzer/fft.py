9d55ea55fe72e09730cc893d048ff357a0f88552
jeanminet <jean.minet@gmail.com>
2023-12-07 14:12:32 +0100

    alpha15 signal-analyzer start python

---
+++ examples/alpha15/signal-analyzer/fft.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import math
+import numpy as np
+
+from koheron import command
+
+class FFT(object):
+    def __init__(self, client):
+        self.client = client
+        self.n_pts = self.get_fft_size()
+
+    def get_fs(self):
+        return self.get_control_parameters()[2]
+
+    @command()
+    def get_fft_size(self):
+        return self.client.recv_uint32()
+
+    @command()
+    def get_cycle_index(self):
+        return self.client.recv_tuple('II')
+
+    @command()
+    def set_offset(self, offset_real, offset_imag):
+        pass
+
+    @command()
+    def set_input_channel(self, channel):
+        pass
+
+    def set_demod(self, data):
+        @command()
+        def set_demod_buffer(self, data):
+            pass
+        data1 = np.uint32(np.mod(np.floor(32768 * data[0, :]) + 32768, 65536) + 32768)
+        data2 = np.uint32(np.mod(np.floor(32768 * data[1, :]) + 32768, 65536) + 32768)
+        set_demod_buffer(self, data1 + (data2 << 16))
+
+    @command()
+    def read_psd(self):
+        return self.client.recv_array(self.n_pts//2, dtype='float32')
+
+    @command()
+    def read_psd_raw(self):
+        return self.client.recv_array(self.n_pts//2, dtype='float32')
+
+    # DDS
+
+    @command()
+    def set_dds_freq(self, channel, freq):
+        pass
+
+    @command()
+    def get_control_parameters(self):
+        return self.client.recv_tuple('dddIdd')
+
+    @command()
+    def get_adc_raw_data(self, n_avg):
+        return self.client.recv_array(2, dtype='int32')
+
+    # Demodulation
+
+    @command(classname='Demodulator')
+    def get_fifo_length(self):
+        return self.client.recv_uint32()
+
+    @command(classname='Demodulator')
+    def get_vector(self, n_pts):
+        return self.client.recv_vector(dtype='int32')
+
+    @command()
+    def set_fft_window(self, window_name):
+        pass
+
+    @command(classname="ClockGenerator")
+    def set_reference_clock(self, val):
+        pass
--- examples/alpha15/signal-analyzer/test.py
+++ examples/alpha15/signal-analyzer/test.py
@@ -9,22 +9,44 @@ import os
 import time
 
 from decimator import Decimator
+from fft import FFT
 from koheron import connect
 
 host = os.getenv('HOST', '192.168.1.113')
 client = connect(host, 'signal-analyzer')
-driver = Decimator(client)
+decimator = Decimator(client)
+fft = FFT(client)
 
-n = 8192
-m = 16
-data = np.zeros(n*m)
+# n = 8192
+# m = 16
+# data = np.zeros(n*m)
 
-driver.read_adc()
-driver.read_adc()
+# decimator.read_adc()
+# decimator.read_adc()
 
-for i in range(m):
-    print(i)
-    data[8192*i:8192*i+8192] = driver.read_adc()
+# for i in range(m):
+#     print(i)
+#     data[8192*i:8192*i+8192] = driver.read_adc()
 
-plt.plot(data)
+# plt.plot(data)
+# plt.show()
+
+
+fft.set_fft_window(0)
+fft.set_input_channel(0)
+
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot(111)
+ax.set_xlim([0, 125])
+ax.set_ylim([0, 40])
+
+freqs = np.arange(driver.n_pts/2) * 15. / 8192
+
+psd = np.sqrt(driver.read_psd()) # V/rtHz
+ax.plot(freqs, psd * 1e9, label='psd')
+
+ax.set_xlabel('Frequency (MHz)')
+ax.set_ylabel('Voltage noise density (U.A.)')
+
+ax.legend()
 plt.show()
