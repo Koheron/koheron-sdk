f1ed7515f8d3c0ac33353d4710f77a692610ff8d
Rashed <r.sarwar87@gmail.com>
2024-04-13 19:45:40 +0100

    add allow instrumentwise overlay patches

--- os/os.mk
+++ os/os.mk
@@ -185,7 +185,8 @@ $(TMP_OS_PATH)/overlay/pl.dtsi: $(TMP_FPGA_PATH)/$(NAME).xsa $(DTREE_PATH) $(PAT
 	mkdir -p $(@D)
 	$(HSI) $(FPGA_PATH)/hsi/devicetree.tcl $(NAME) $(PROC) $(DTREE_PATH) $(VIVADO_VER) $(TMP_OS_PATH)/hard $(TMP_OS_PATH)/overlay $(TMP_FPGA_PATH)/$(NAME).xsa $(BOOT_MEDIUM)
 	cp -R $(TMP_OS_PATH)/overlay $(TMP_OS_PATH)/overlay.orig
-	patch -d $(TMP_OS_PATH) -p -0 < $(PATCHES)/overlay.patch
+	[[ -f $(PROJECT_PATH)/overlay.patch ]] || touch $(PROJECT_PATH)/overlay.patch
+	patch -d $(TMP_OS_PATH) -p -0 < $(PROJECT_PATH)/overlay.patch
 	@echo [$@] OK
 
 $(TMP_OS_PATH)/devicetree/system-top.dts: $(TMP_FPGA_PATH)/$(NAME).xsa $(DTREE_PATH) $(PATCHES)/devicetree.patch
