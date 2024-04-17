85fc4912a5f78ffb9c9f073a1ba83be98287adb2
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2024-04-17 12:06:32 +0200

    Try fix CI

--- setup/install_web_debian.sh
+++ setup/install_web_debian.sh
@@ -1,6 +1,4 @@
 apt install -y nodejs
-apt install -y nodejs-dev
-apt install -y node-gyp
-apt install -y node-typescript
+#apt install -y node-typescript
 apt install -y npm # npm installed with nodejs
 
