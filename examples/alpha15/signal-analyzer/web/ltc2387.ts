369baddc7b5d0e3efa36b6daf67fc45d09c22310
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2024-07-16 18:13:57 +0200

    Start input range select buttons

---
+++ examples/alpha15/signal-analyzer/web/ltc2387.ts
@@ -0,0 +1,23 @@
+// Interface for Ltc2387 driver
+// (c) Koheron
+
+class Ltc2387 {
+    private driver: Driver;
+    private id: number;
+    private cmds: Commands;
+
+    constructor (private client: Client) {
+        this.driver = this.client.getDriver('Ltc2387');
+        this.id = this.driver.id;
+        this.cmds = this.driver.getCmds();
+    }
+
+    rangeSelect(channel: number, range: number): void {
+        this.client.send(Command(this.id, this.cmds['range_select'], channel, range));
+    }
+
+    inputRange(channel: number, cb: (range: number) => void): void {
+        this.client.readUint32(Command(this.id, this.cmds['input_range']),
+                             (range) => {cb(range)});
+    }
+}
