cdafcf7b429be2cbc8a290f46eabbac84a447565
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2024-07-17 19:19:17 +0200

    First PSD low freq

--- examples/alpha15/signal-analyzer/decimator.hpp
+++ examples/alpha15/signal-analyzer/decimator.hpp
@@ -11,8 +11,16 @@
 #include <thread>
 #include <mutex>
 
+#include <scicpp/signal.hpp>
+
 #include <server/drivers/fifo.hpp>
 #include <boards/alpha15/drivers/clock-generator.hpp>
+
+namespace {
+    namespace sci = scicpp;
+    namespace sig = scicpp::signal;
+    namespace win = scicpp::signal::windows;
+}
 
 class Decimator
 {
@@ -25,10 +33,10 @@ class Decimator
     , clk_gen(ctx.get<ClockGenerator>())
     {
         fs_adc = clk_gen.get_adc_sampling_freq()[0];
-        set_cic_rate(prm::cic_decimation_rate_default);
-        fifo_transfer_duration = n_pts / fs;
-        ctx.log<INFO>("Decimator: FIFO transfer duration = %f s\n", double(fifo_transfer_duration));
-
+        // set_cic_rate(prm::cic_decimation_rate_default);
+        set_cic_rate(64);
+        psd.resize(1 + n_pts / 2);
+        spectrum.window(win::Hann, n_pts);
         start_acquisition();
     }
 
@@ -42,15 +50,27 @@ class Decimator
         cic_rate = rate;
         fs = fs_adc / (2.0f * cic_rate); // Sampling frequency (factor of 2 because of FIR)
         fifo_transfer_duration = n_pts / fs;
+        ctx.log<INFO>("Decimator: FIFO transfer duration = %f s\n",
+                      double(fifo_transfer_duration));
+        spectrum.fs(fs);
         ctl.write<reg::cic_rate>(cic_rate);
     }
 
-    auto read_adc() {
+    auto get_control_parameters() {
+        return std::tuple{fs, fifo_transfer_duration, cic_rate, n_pts};
+    }
+
+    auto read_adc() const {
         return adc_data;
     }
 
+    auto spectral_density() const {
+        return psd;
+    }
+
   private:
-    static constexpr uint32_t n_pts = 8192;
+    // static constexpr uint32_t n_pts = 8192;
+    static constexpr uint32_t n_pts = 16384;
 
     Context& ctx;
     Memory<mem::control>& ctl;
@@ -63,7 +83,7 @@ class Decimator
     float fs_adc, fs;
     float fifo_transfer_duration;
 
-    std::array<uint32_t, n_pts> adc_data;
+    std::array<double, n_pts> adc_data;
 
     // Data acquisition thread
     std::thread acq_thread;
@@ -71,6 +91,10 @@ class Decimator
     std::atomic<bool> acquisition_started{false};
     void acquisition_thread();
     void start_acquisition();
+
+    // Spectrum analyzer
+    sig::Spectrum<double> spectrum;
+    std::vector<double> psd;
 }; // Decimator
 
 inline void Decimator::start_acquisition() {
@@ -90,9 +114,14 @@ inline void Decimator::acquisition_thread() {
         {
             std::lock_guard<std::mutex> lock(mutex);
 
+            const double vrange = 2.048; // TODO Use calibration and range
+            constexpr double nmax = 262144.0; // 2^18
+
             for (uint32_t i = 0; i < n_pts; i++) {
-                adc_data[i] = fifo.read();
+                adc_data[i] = vrange * static_cast<int32_t>(fifo.read()) / nmax / 4096.0;
             }
+
+            psd = spectrum.periodogram<sig::DENSITY, false>(adc_data);
         }
     }
 }
--- examples/alpha15/signal-analyzer/web/decimator-plot/decimator-plot.ts
+++ examples/alpha15/signal-analyzer/web/decimator-plot/decimator-plot.ts
@@ -9,7 +9,9 @@ class DecimatorPlot {
     public yLabel: string = "Voltage (V)";
     private peakDatapoint: number[];
 
-    constructor(document: Document, private decimator: Decimator, private plotBasics: PlotBasics) {
+    constructor(document: Document,
+                private decimator: Decimator,
+                private plotBasics: PlotBasics) {
         this.n_pts = 8192;
         this.peakDatapoint = [];
         this.plot_data = [];
@@ -18,13 +20,14 @@ class DecimatorPlot {
     }
 
     updatePlot() {
-        this.decimator.read_adc( (data: Int32Array) => {
+        this.decimator.read_adc( (data: Float64Array) => {
             for (let i: number = 0; i <= this.n_pts; i++) {
                 this.plot_data[i] = [i, data[i]];
             }
 
             this.plotBasics.redraw(this.plot_data, this.n_pts, this.peakDatapoint, this.yLabel, () => {
-                requestAnimationFrame( () => { this.updatePlot(); } );
+                setTimeout(() => { this.updatePlot(); }, 
+                           this.decimator.status.tx_duration * 1000);
             });
         });
     }
