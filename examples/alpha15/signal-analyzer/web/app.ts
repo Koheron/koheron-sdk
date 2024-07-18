1fa5cd83ebfb3c2af47ab9eb1cda15263ba48ba3
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2024-07-18 19:20:11 +0200

    Clean up

--- examples/alpha15/signal-analyzer/web/app.ts
+++ examples/alpha15/signal-analyzer/web/app.ts
@@ -2,8 +2,6 @@ class App {
     private imports: Imports;
     public plot: Plot;
     private plotBasics: PlotBasics;
-    public plotDecimator: DecimatorPlot;
-    private plotBasicsDecimator: PlotBasics;
     private fft: FFT;
     public fftApp: FFTApp;
     private decimator: Decimator;
@@ -24,17 +22,10 @@ class App {
     private y_min: number;
     private y_max: number;
 
-    private decim_n_pts: number;
-    private decim_x_min: number;
-    private decim_x_max: number;
-    private decim_y_min: number;
-    private decim_y_max: number;
-
     constructor(window: Window,
                 document: Document,
                 ip: string,
-                plot_placeholder: JQuery,
-                decim_plot_placeholder: JQuery) {
+                plot_placeholder: JQuery) {
         let sockpoolSize: number = 10;
         let client = new Client(ip, sockpoolSize);
 
@@ -51,7 +42,7 @@ class App {
 
                 this.fft.init( () => {
                     this.decimator.init( () => {
-                        this.initApp(plot_placeholder, decim_plot_placeholder);
+                        this.initApp(plot_placeholder);
                     });
                 });
             });
@@ -60,13 +51,12 @@ class App {
         window.onbeforeunload = () => { client.exit(); };
     }
 
-    private initApp(plot_placeholder: JQuery,
-                    decim_plot_placeholder: JQuery) {
+    private initApp(plot_placeholder: JQuery) {
         this.fftApp = new FFTApp(document, this.fft);
 
         // FFT plot
         this.n_pts = this.fft.fft_size / 2;
-        this.x_min = 10;
+        this.x_min = 100;
         this.x_max = this.fft.status.fs / 1E6 / 2;
         this.y_min = -150;
         this.y_max = 50;
@@ -75,21 +65,8 @@ class App {
                                          this.n_pts,
                                          this.x_min, this.x_max,
                                          this.y_min, this.y_max,
-                                         this.fft, "", "Frequency (MHz)");
+                                         this.fft, "", "Frequency (Hz)");
         this.plot = new Plot(document, this.fft, this.decimator, this.plotBasics);
-
-        // Decimator plot
-        this.decim_n_pts = 8192;
-        this.decim_x_min = 0;
-        this.decim_x_max = 8192;
-        this.decim_y_min = -2;
-        this.decim_y_max = 2;
-        this.plotBasicsDecimator = new PlotBasics(document, decim_plot_placeholder,
-                                                  this.decim_n_pts,
-                                                  this.decim_x_min, this.decim_x_max,
-                                                  this.decim_y_min, this.decim_y_max,
-                                                  this.decimator, "", "Time (s)");
-        this.plotDecimator = new DecimatorPlot(document, this.decimator, this.plotBasicsDecimator);
 
         this.temperatureSensorApp = new TemperatureSensorApp(document, this.temperatureSensor);
         this.powerMonitorApp = new PowerMonitorApp(document, this.powerMonitor);
@@ -100,6 +77,4 @@ class App {
 
 }
 
-let app = new App(window, document, location.hostname,
-                  $('#plot-placeholder'),
-                  $('#decim-plot-placeholder'));
+let app = new App(window, document, location.hostname, $('#plot-placeholder'));
--- examples/alpha15/signal-analyzer/web/plot/plot.ts
+++ examples/alpha15/signal-analyzer/web/plot/plot.ts
@@ -2,12 +2,10 @@
 // (c) Koheron
 
 class Plot {
-    private n_pts_very_slow: number;
     private n_pts_slow: number;
     private n_pts_fast: number;
     public n_pts: number;
 
-    private n_stop_very_slow: number;
     private n_start_fast: number;
     private n_start_slow: number;
     private n_stop_slow: number;
@@ -25,22 +23,17 @@ class Plot {
                 private decimator: Decimator,
                 private plotBasics: PlotBasics) {
         this.n_pts_fast = this.fft.fft_size / 2;
-        this.n_pts_very_slow = 1 + this.decimator.status.n_pts_full / 2;
         this.n_pts_slow = 1 + this.decimator.status.n_pts / 2;
 
-        this.n_stop_very_slow = 100;
-        this.n_start_slow = 10;
+        this.n_start_slow = 1;
+        this.n_stop_slow = 600;
         this.n_start_fast = 55;
-        this.n_stop_slow = 150;
 
-        let size_very_slow = this.n_pts_very_slow - this.n_stop_very_slow;
         let size_slow = this.n_pts_slow - this.n_stop_slow - this.n_start_slow;
         let size_fast = this.n_pts_fast - this.n_start_fast;
 
-        this.n0_slow = size_very_slow + 1;
-        this.n0_fast = size_very_slow + size_slow + 1;
-
-        this.n_pts = size_very_slow + size_slow + size_fast;
+        this.n0_slow = 0;
+        this.n0_fast = size_slow + 1;
         
         this.peakDatapoint = [];
         this.plot_data = [];
@@ -51,38 +44,19 @@ class Plot {
     updatePlot() {
         this.fft.read_psd( (fast_psd: Float32Array) => {
             this.decimator.spectral_density( (slow_psd: Float64Array) => {
-                this.decimator.spectral_density_full( (very_slow_psd: Float64Array) => {
-                    let yUnit: string = (<HTMLInputElement>document.querySelector(".unit-input:checked")).value;
-                    
-                    // TODO update very slow PSD at a lower rate ...
-                    this.updateVerySlowPsdPlot(very_slow_psd, yUnit);
-                    this.updateSlowPsdPlot(slow_psd, yUnit);
-                    this.updateFastPsdPlot(fast_psd, yUnit);
+                let yUnit: string = (<HTMLInputElement>document.querySelector(".unit-input:checked")).value;
 
-                    this.plotBasics.redraw(this.plot_data,
-                                        this.n_pts,
-                                        this.peakDatapoint,
-                                        this.yLabel, () => {
-                        requestAnimationFrame( () => { this.updatePlot(); } );
-                    });
+                this.updateSlowPsdPlot(slow_psd, yUnit);
+                this.updateFastPsdPlot(fast_psd, yUnit);
+
+                this.plotBasics.redraw(this.plot_data,
+                                    this.n_pts,
+                                    this.peakDatapoint,
+                                    this.yLabel, () => {
+                    requestAnimationFrame( () => { this.updatePlot(); } );
                 });
             });
         });
-    }
-
-    private updateVerySlowPsdPlot(very_slow_psd: Float64Array, yUnit: string) {
-        let fstep = this.decimator.status.fs_full / 2 / this.n_pts_very_slow;
-
-        // console.log(this.n_pts_very_slow)
-        // console.log(fstep)
-        console.log(very_slow_psd)
-        // console.log(this.n_pts_very_slow - this.n_stop_very_slow)
-
-        for (let i: number = 0; i <= this.n_pts_very_slow - this.n_stop_very_slow; i++) {
-            let freq = (i + 1) * fstep;
-            let convertedSlowPsd = this.convertValue(very_slow_psd[i], yUnit);
-            this.plot_data[i] = [freq, convertedSlowPsd];
-        }
     }
 
     private updateSlowPsdPlot(slow_psd: Float64Array, yUnit: string) {
@@ -100,7 +74,7 @@ class Plot {
 
         if (max_x != this.plotBasics.x_max) { // Sampling frequency has changed
             this.plotBasics.x_max = max_x;
-            this.plotBasics.setRangeX(10, this.plotBasics.x_max);
+            this.plotBasics.setRangeX(100, this.plotBasics.x_max);
             this.plotBasics.setLogX();
         }
 
