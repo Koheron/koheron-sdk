75bf8cf86ff797f558639c9850dd31028316c326
jeanminet <jean.minet@gmail.com>
2025-09-22 17:44:36 +0200

    clean fft config.mk

--- examples/alpha250/fft/config.mk
+++ examples/alpha250/fft/config.mk
@@ -5,22 +5,14 @@ BOARD_PATH := $(SDK_PATH)/boards/alpha250
 
 CONFIG_YML = $(PROJECT_PATH)/config.yml
 
-XDC := $(BOARD_PATH)/config/ports.xdc
 
-CORES += $(SDK_PATH)/fpga/cores/axi_ctl_register_v1_0
-CORES += $(SDK_PATH)/fpga/cores/axi_sts_register_v1_0
 CORES += $(SDK_PATH)/fpga/cores/axis_constant_v1_0
 CORES += $(SDK_PATH)/fpga/cores/latched_mux_v1_0
-CORES += $(SDK_PATH)/fpga/cores/edge_detector_v1_0
-CORES += $(SDK_PATH)/fpga/cores/comparator_v1_0
-CORES += $(SDK_PATH)/fpga/cores/unrandomizer_v1_0
 CORES += $(SDK_PATH)/fpga/cores/psd_counter_v1_0
 
 DRIVERS += $(PROJECT_PATH)/fft.hpp
 
-WEB_FILES += $(SDK_PATH)/web/koheron.ts
 WEB_FILES += $(SDK_PATH)/web/jquery.flot.d.ts
-WEB_FILES += $(SDK_PATH)/web/main.css
 WEB_FILES += $(SDK_PATH)/web/dds-frequency/dds-frequency.html
 WEB_FILES += $(SDK_PATH)/web/dds-frequency/dds-frequency.ts
 WEB_FILES += $(PROJECT_PATH)/web/index.html
