99a3a9db6816ae19f24dbb4e2571d0ce8cf9e462
Jean Minet <jean.minet@koheron.com>
2025-09-26 22:32:58 +0200

    alpha250-4 adc-bram compatible with V1

---
+++ examples/alpha250-4/adc-bram/config.mk
@@ -0,0 +1,13 @@
+NAME := adc-bram
+VERSION := 0.2.1
+
+BOARD_PATH := $(SDK_PATH)/boards/alpha250-4
+
+CONFIG_YML = $(PROJECT_PATH)/config.yml
+
+CORES += $(SDK_PATH)/examples/alpha250/adc-dac-bram/address_counter_v1_0
+
+include $(SDK_PATH)/boards/alpha250-4/drivers/drivers.mk
+DRIVERS += $(PROJECT_PATH)/adc_bram.hpp
+
+WEB_FILES += $(SDK_PATH)/web/index.html
