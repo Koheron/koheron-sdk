07b550598f35f577e4ce1a0d0187e303f72e502a
Jean Minet <jean.minet@koheron.com>
2025-09-26 22:18:36 +0200

    alpha250-4/fft compatible with V1

---
+++ boards/alpha250-4/drivers/drivers.mk
@@ -0,0 +1,11 @@
+DRIVERS += $(SDK_PATH)/boards/alpha250-4/drivers/common.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250-4/drivers/clock-generator.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250-4/drivers/ltc2157.hpp
+
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/eeprom.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/gpio-expander.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/temperature-sensor.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/power-monitor.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/precision-adc.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/precision-dac.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/spi-config.hpp
---
+++ examples/alpha250-4/fft/config_1g.mk
@@ -0,0 +1,2 @@
+include $(PROJECT_PATH)/config.mk
+BOARD_PATH := $(SDK_PATH)/boards/alpha250-4-1g
