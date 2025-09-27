8d64ae0d9d76546c8b243b03777762ee6754969b
Jean Minet <jean.minet@koheron.com>
2025-09-28 00:17:24 +0200

    clean

--- install_eigen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=3.3.9
-
-wget -P /tmp https://gitlab.com/libeigen/eigen/-/archive/${VERSION}/eigen-${VERSION}.zip
-unzip -o /tmp/eigen-${VERSION}.zip -d /tmp
-cp -r /tmp/eigen-${VERSION}/unsupported /usr/include
-cp -r /tmp/eigen-${VERSION}/Eigen /usr/include
-
