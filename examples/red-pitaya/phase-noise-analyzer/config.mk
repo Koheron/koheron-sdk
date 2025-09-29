a25f34355d85f61e3bc53eba4c863144f61ea779
jeanminet <jean.minet@gmail.com>
2025-09-29 12:39:14 +0200

    red-pitaya phase-noise-analyzer

---
+++ examples/red-pitaya/phase-noise-analyzer/config.mk
@@ -0,0 +1,22 @@
+NAME := phase-noise-analyzer
+VERSION := 0.1.0
+
+BOARD_PATH := $(SDK_PATH)/boards/red-pitaya
+
+CONFIG_YML = $(PROJECT_PATH)/config.yml
+
+XDC += $(SDK_PATH)/boards/red-pitaya/config/ports.xdc
+XDC += $(SDK_PATH)/boards/red-pitaya/config/clocks.xdc
+
+include $(SDK_PATH)/boards/red-pitaya/cores/cores.mk
+CORES += $(SDK_PATH)/fpga/cores/latched_mux_v1_0
+CORES += $(SDK_PATH)/fpga/cores/tlast_gen_v1_0
+CORES += $(SDK_PATH)/fpga/cores/axis_lfsr_v1_0
+CORES += $(SDK_PATH)/fpga/cores/phase_unwrapper_v1_0
+CORES += $(SDK_PATH)/fpga/cores/boxcar_filter_v1_0
+
+DRIVERS += $(SDK_PATH)/server/drivers/common.hpp
+DRIVERS += $(PROJECT_PATH)/dds.hpp
+DRIVERS += $(PROJECT_PATH)/dma.hpp
+
+WEB_FILES += $(SDK_PATH)/web/index.html
