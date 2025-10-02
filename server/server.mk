ae6718082b15a5b536899b281d5084057cd1585e
thomas <th.vanderbruggen@gmail.com>
2025-10-02 19:41:19 +0200

    Add -fno-exceptions

--- server/server.mk
+++ server/server.mk
@@ -135,7 +135,7 @@ SERVER_CCXXFLAGS += -Wuninitialized  -Wmissing-declarations
 SERVER_CCXXFLAGS += -Wno-psabi
 SERVER_CCXXFLAGS += -I$(TMP_SERVER_PATH) -I$(SERVER_PATH)/external_libs -I$(SERVER_PATH)/core -I$(SDK_PATH) -I$(SERVER_PATH)/context -I$(SERVER_PATH)/drivers -I$(PROJECT_PATH)
 SERVER_CCXXFLAGS += -DKOHERON_VERSION=$(KOHERON_VERSION).$(shell git rev-parse --short HEAD) -DINSTRUMENT_NAME=$(NAME) -DKOHERON_SERVER_BUILD
-SERVER_CCXXFLAGS += -O3 -fno-math-errno
+SERVER_CCXXFLAGS += -O3 -fno-math-errno -fno-exceptions
 SERVER_CCXXFLAGS += -MMD -MP -static-libstdc++ $(GCC_FLAGS)
 SERVER_CCXXFLAGS += -std=c++20 -pthread
 
