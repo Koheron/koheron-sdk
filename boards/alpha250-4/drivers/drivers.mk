e5c497696a20e14a9247611c63922fd75d3753df
thomas <th.vanderbruggen@gmail.com>
2025-10-03 10:53:59 +0200

    Fix ALPHA250-4 includes

--- boards/alpha250-4/drivers/drivers.mk
+++ boards/alpha250-4/drivers/drivers.mk
@@ -4,8 +4,10 @@ DRIVERS += $(SDK_PATH)/boards/alpha250-4/drivers/ltc2157.hpp
 
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/eeprom.hpp
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/gpio-expander.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/gpio-expander.cpp
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/temperature-sensor.hpp
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/power-monitor.hpp
+DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/power-monitor.cpp
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/precision-adc.hpp
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/precision-dac.hpp
 DRIVERS += $(SDK_PATH)/boards/alpha250/drivers/spi-config.hpp
