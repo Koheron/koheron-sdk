62fbefcc064c4c4f0f64a1d3a2a9500e9b42d371
thomas <th.vanderbruggen@gmail.com>
2025-10-07 14:56:44 +0200

    Add alpha15 precision-dac.cpp

---
+++ boards/alpha15/drivers/precision-dac.cpp
@@ -0,0 +1,60 @@
+#include "./precision-dac.hpp"
+#include "./eeprom.hpp"
+
+#include "server/context/context.hpp"
+
+#include <cmath>
+
+PrecisionDac::PrecisionDac(Context& ctx_)
+: ctx(ctx_)
+, eeprom(ctx.get<Eeprom>())
+{}
+
+void PrecisionDac::init() {
+    eeprom.read<eeprom_map::precision_dac_calib::offset>(cal_coeffs);
+    auto& ctl = ctx.mm.get<mem::control>();
+    ctl.write<reg::precision_dac_ctl>((regs::RESET << 1));
+    ctl.write<reg::precision_dac_ctl>((regs::WRITE_UPDATE << 1) + enable);
+    set_dac_value(0, 0);
+    set_dac_value(1, 0);
+    set_dac_value(2, 0);
+    set_dac_value(3, 0);
+}
+
+void PrecisionDac::set_dac_value_volts(uint32_t channel, float voltage) {
+    if (channel >= n_dacs) {
+        ctx.log<ERROR>("PrecisionDac::set_dac_value_volts invalid channel");
+        return;
+    }
+
+    if (std::isnan(voltage)) {
+        ctx.log<ERROR>("PrecisionDac::set_dac_value_volts voltage is NaN");
+        return;
+    }
+
+    if (voltage > 2.5f) {
+        voltage = 2.5f;
+    }
+
+    if (voltage < 0.0f) {
+        voltage = 0.0f;
+    }
+
+    values_volt[channel] = voltage;
+
+    uint32_t code = uint32_t(std::round(cal_coeffs[2 * channel] * voltage + cal_coeffs[2 * channel + 1]));
+    set_dac_value(channel, code);
+}
+
+void PrecisionDac::set_dac_value(uint32_t channel, uint32_t code) {
+    dac_values[channel & 0b11] = code;
+    auto& ctl = ctx.mm.get<mem::control>();
+    ctl.write<reg::precision_dac_data0>((dac_values[1] << 16) + (dac_values[0] & 0xFFFF));
+    ctl.write<reg::precision_dac_data1>((dac_values[3] << 16) + (dac_values[2] & 0xFFFF));
+}
+
+int32_t PrecisionDac::set_calibration_coeffs(const std::array<float, 2 * n_dacs>& new_coeffs) {
+    cal_coeffs = new_coeffs;
+    static_assert(2 * n_dacs * sizeof(float) <= eeprom_map::precision_dac_calib::range, "");
+    return eeprom.write<eeprom_map::precision_dac_calib::offset>(cal_coeffs);
+}
