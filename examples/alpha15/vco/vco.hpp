1d3f8f104c7583ade2efd559a1b68658fd5f1b81
thomas <th.vanderbruggen@gmail.com>
2025-10-16 16:12:28 +0200

    Update VCO

--- examples/alpha15/vco/vco.hpp
+++ examples/alpha15/vco/vco.hpp
@@ -2,22 +2,17 @@
 ///
 /// (c) Koheron
 
-#ifndef __DRIVERS_DUAL_DDS_HPP__
-#define __DRIVERS_DUAL_DDS_HPP__
+#ifndef __ALPHA15_VCO_HPP__
+#define __ALPHA15_VCO_HPP__
 
-#include <context.hpp>
-#include <atomic>
-#include <thread>
-#include <chrono>
+#include "server/hardware/memory_manager.hpp"
+
+#include <cstdint>
 
 class VCO
 {
   public:
-    VCO(Context& ctx)
-    : ctl(ctx.mm.get<mem::control>())
-    , sts(ctx.mm.get<mem::status>())
-    {
-    }
+    VCO() : ctl(hw::get_memory<mem::control>()){}
 
     void set_dds_freq(uint32_t channel, double freq_hz) {
         constexpr double factor = (uint64_t(1) << 48) / double(prm::adc_clk);
@@ -30,9 +25,6 @@ class VCO
 
   private:
     hw::Memory<mem::control>& ctl;
-    hw::Memory<mem::status>& sts;
-
 };
 
-
-#endif // __DRIVERS_VCO_HPP__
+#endif // __ALPHA15_VCO_HPP__
