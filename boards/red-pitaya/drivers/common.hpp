2c54140d853be08a6f2a9e93fbace578c670682c
thomas <th.vanderbruggen@gmail.com>
2025-10-24 17:54:11 +0200

    Use LedController for RedP Common

--- boards/red-pitaya/drivers/common.hpp
+++ boards/red-pitaya/drivers/common.hpp
@@ -7,9 +7,11 @@
 
 #include "server/runtime/syslog.hpp"
 #include "server/hardware/memory_manager.hpp"
+#include "server/drivers/leds-control.hpp"
 
 #include <cstring>
 #include <array>
+#include <memory>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -19,20 +21,24 @@ class Common
 {
   public:
     Common()
-    : ctl(hw::get_memory<mem::control>())
-    , sts(hw::get_memory<mem::status>())
-    {}
-
-    void set_led(uint32_t value) {
-        ctl.write<reg::led>(value);
+    : leds(std::make_unique<LedsController>())
+    , ctl(hw::get_memory<mem::control>())
+    {
+        leds->setter([&](uint32_t v){
+            ctl.write<reg::led>(v);
+        });
     }
 
-    uint32_t get_led() {
-        return ctl.read<reg::led>();
+    ~Common() {
+        leds->stop_blink();
+    }
+
+    void set_led(uint32_t value) {
+        leds->set_led(value);
     }
 
     void init() {
-        ip_on_leds();
+        leds->start_blink();
     };
 
     std::string get_instrument_config() {
@@ -40,35 +46,12 @@ class Common
     }
 
     void ip_on_leds() {
-        struct ifaddrs* addrs = nullptr;
-        if (getifaddrs(&addrs) != 0 || !addrs) return;
-
-        // Turn all the LEDs ON
-        set_led(255);
-
-        const char* preferred[] = {"end0", "eth0"};
-        for (const char* want : preferred) {
-            for (auto* it = addrs; it; it = it->ifa_next) {
-                if (!it->ifa_addr || it->ifa_addr->sa_family != AF_INET) continue;
-                if (std::strcmp(it->ifa_name, want) != 0) continue;
-
-                auto* pAddr = reinterpret_cast<sockaddr_in*>(it->ifa_addr);
-                logf("Interface {} found: {}\n", it->ifa_name, inet_ntoa(pAddr->sin_addr));
-                uint32_t ip = htonl(pAddr->sin_addr.s_addr);
-                set_led(ip);
-                freeifaddrs(addrs);
-                return;
-            }
-        }
-
-        // Neither end0 nor eth0 had an IPv4; keep LEDs as-is (optional: log)
-        log("No IPv4 on end0/eth0\n");
-        freeifaddrs(addrs);
+        leds->ip_on_leds();
     }
 
   private:
+    std::unique_ptr<LedsController> leds;
     hw::Memory<mem::control>& ctl;
-    hw::Memory<mem::status>& sts;
 };
 
 #endif // __DRIVERS_COMMON_HPP__
