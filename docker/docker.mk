bd9f9555f19b71c52c8fb3861568895d155b5455
Yann Le Coq <lecoqya@univ-grenoble-alpes.fr>
2026-03-12 14:37:58 +0100

    docker: mount PROJECT_PATH so out-of-tree projects can build
    When CONFIG points to a project outside the SDK directory tree,
    the Docker container cannot access the project files (config.yml,
    block_design.tcl, cores/, etc.) because only SDK_FULL_PATH is
    bind-mounted.
    
    Add a second bind-mount for PROJECT_PATH. When the project lives
    inside the SDK, this is harmless (mounting a subdirectory of an
    already-mounted path is a no-op for Docker).

--- docker/docker.mk
+++ docker/docker.mk
@@ -2,5 +2,10 @@ DOCKER :=
 ifeq ($(BUILD_METHOD),docker)
 	UID = $(shell id -u)
 	GID = $(shell id -g)
-	DOCKER = docker run --rm -v $(SDK_FULL_PATH):/home/containeruser/wkspace:Z -u $(UID):$(GID) -w /home/containeruser/wkspace gnu-gcc-9.5
+	DOCKER = docker run --rm \
+		-v $(SDK_FULL_PATH):/home/containeruser/wkspace:Z \
+		-v $(PROJECT_PATH):$(PROJECT_PATH):Z \
+		-u $(UID):$(GID) \
+		-w /home/containeruser/wkspace \
+		gnu-gcc-9.5
 endif
