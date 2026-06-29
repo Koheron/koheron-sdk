9dc78792fcd391d499af2db67e02500e8685d5c9
jeanminet <jean.minet@gmail.com>
2026-06-29 14:31:11 +0200

    Fail instrument install on unzip errors

--- os/api/install_instrument.sh
+++ os/api/install_instrument.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
 NAME=$1
 LIVE_DIRNAME=$2
 
-/bin/rm -rf ${LIVE_DIRNAME}
-/bin/mkdir -p ${LIVE_DIRNAME}
+/bin/rm -rf -- "$LIVE_DIRNAME"
+/bin/mkdir -p -- "$LIVE_DIRNAME"
 
 /bin/systemctl stop koheron-server.service
-/usr/bin/unzip -o /usr/local/instruments/${NAME}.zip -d ${LIVE_DIRNAME}
+/usr/bin/unzip -o "/usr/local/instruments/${NAME}.zip" -d "$LIVE_DIRNAME"
 /bin/systemctl start koheron-server.service
 /bin/systemctl start koheron-server-init.service
