2fb1d6eb2d3ab63620c22f7e2f578898c62b9fc1
jeanminet <jean.minet@gmail.com>
2026-06-29 14:35:44 +0200

    Return error status on instrument install failure

--- os/api/__init__.py
+++ os/api/__init__.py
@@ -307,16 +307,18 @@ def get_instruments_details():
 def run_instrument(name):
     zip_filename = '{}.zip'.format(name)
     filename = os.path.join(app.instruments_dirname, secure_filename(zip_filename))
+
+    if not os.path.exists(filename):
+        return make_response('Instrument %s not found' % zip_filename, 404)
+
     is_default = app.is_default_instrument(filename, app.instruments_dirname, app.default_filename)
     instrument_dict = app.get_instrument_dict(filename, is_default, app.version_filename)
     status = app.run_instrument(filename, app.live_instrument_dirname, instrument_dict)
 
     if status == 'success':
-        response = 'Instrument %s successfully installed' % zip_filename
-    else:
-        response = 'Failed to install instrument %s' % zip_filename
+        return make_response('Instrument %s successfully installed' % zip_filename)
 
-    return make_response(response)
+    return make_response('Failed to install instrument %s' % zip_filename, 500)
 
 @app.route('/api/instruments/delete/<name>', methods=['GET'])
 def delete_instrument(name):
